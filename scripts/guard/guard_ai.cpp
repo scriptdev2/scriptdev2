1227e1058ec2a9b3663500c7800a8e6c7bd42109
NoFantasy <nofantasy@nf.no>
2007-08-03 07:08:24 +0000

    [101] Fixed : support core change 4232 IsHostileToPlayers
    Fixed : support core change 4232 IsHostileToPlayers
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@101 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/guard/guard_ai.cpp
+++ scripts/guard/guard_ai.cpp
@@ -63,7 +63,7 @@ void guardAI::MoveInLineOfSight(Unit *who)
     if (!who || m_creature->getVictim())
         return;
 
-    if (who->isTargetableForAttack() && who->isInAccessablePlaceFor(m_creature) && (m_creature->IsHostileTo(who) || who->IsHostileToPlayer()))
+    if (who->isTargetableForAttack() && who->isInAccessablePlaceFor(m_creature) && (m_creature->IsHostileTo(who) || who->IsHostileToPlayers()))
     {
         float attackRadius = m_creature->GetAttackDistance(who);
         if (m_creature->IsWithinDistInMap(who, attackRadius) && m_creature->GetDistanceZ(who) <= CREATURE_Z_ATTACK_RANGE && m_creature->IsWithinLOSInMap(who))
