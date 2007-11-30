56d735f8ad77fea0d61e9cf87d44959c74e8d9a8
NoFantasy <nofantasy@nf.no>
2007-11-30 21:10:30 +0000

    [181] Added: npc lantresor, the tale of the blademaster (quest 10107/10108)
    Added: npc lantresor, the tale of the blademaster (quest 10107/10108)
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@181 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/zone/nagrand/npcs_nagrand.cpp
+++ scripts/zone/nagrand/npcs_nagrand.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: Npcs_Nagrand
 SD%Complete: 85
-SDComment: Nagrand NPCs, Altruis gossip incomplete (but working) due to missing/unknown text id's
+SDComment: Nagrand NPCs, Altruis gossip incomplete (but working) due to missing/unknown text id's.
 EndScriptData */
 
 #include "../../sc_defines.h"
@@ -116,6 +116,74 @@ bool QuestAccept_npc_altruis_the_sufferer(Player *player, Creature *creature, Qu
     }
     return true;
 }
+/*######
+## npc_lantresor_of_the_blade
+######*/
+
+bool GossipHello_npc_lantresor_of_the_blade(Player *player, Creature *_Creature)
+{
+    if (_Creature->isQuestGiver())
+        player->PrepareQuestMenu( _Creature->GetGUID() );
+
+    if (player->GetQuestStatus(10107) == QUEST_STATUS_INCOMPLETE ||
+        player->GetQuestStatus(10108) == QUEST_STATUS_INCOMPLETE)
+        player->ADD_GOSSIP_ITEM( 0, "Talk to me", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
+
+    player->PlayerTalkClass->SendGossipMenu(_Creature->GetNpcTextId(), _Creature->GetGUID());
+
+    return true;
+}
+
+bool GossipSelect_npc_lantresor_of_the_blade(Player *player, Creature *_Creature, uint32 sender, uint32 action)
+{
+    switch (action)
+    {
+        case GOSSIP_ACTION_INFO_DEF:
+            player->ADD_GOSSIP_ITEM( 0, "Death? No...", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            player->SEND_GOSSIP_MENU(9361, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+1:
+            player->ADD_GOSSIP_ITEM( 0, "Easy, you're an Orc.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            player->SEND_GOSSIP_MENU(9362, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+2:
+            player->ADD_GOSSIP_ITEM( 0, "Half-orc? What's the other half then?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            player->SEND_GOSSIP_MENU(9363, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+3:
+            player->ADD_GOSSIP_ITEM( 0, "Sorry, i've never seen it, i don't belive you.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);
+            player->SEND_GOSSIP_MENU(9364, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+4:
+            player->ADD_GOSSIP_ITEM( 0, "Ok, ok! I'm only here to ask for your help.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 5);
+            player->SEND_GOSSIP_MENU(9365, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+5:
+            player->ADD_GOSSIP_ITEM( 0, "We want the attacks from the Boulderfist clan to stop!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 6);
+            player->SEND_GOSSIP_MENU(9366, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+6:
+            player->ADD_GOSSIP_ITEM( 0, "Our leaders are demanding it!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 7);
+            player->SEND_GOSSIP_MENU(9367, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+7:
+            player->ADD_GOSSIP_ITEM( 0, "Ok, go ahead and tell.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 8);
+            player->SEND_GOSSIP_MENU(9368, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+8:
+            player->ADD_GOSSIP_ITEM( 0, "So be it, i'm prepared!", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9);
+            player->SEND_GOSSIP_MENU(9369, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+9:
+            player->PlayerTalkClass->CloseGossip();
+            if (player->GetQuestStatus(10107) == QUEST_STATUS_INCOMPLETE)
+                player->CompleteQuest(10107);
+            if (player->GetQuestStatus(10108) == QUEST_STATUS_INCOMPLETE)
+                player->CompleteQuest(10108);
+            break;
+    }
+    return true;
+}
 
 /*######
 ## AddSC
@@ -130,5 +198,11 @@ void AddSC_npcs_nagrand()
     newscript->pGossipHello = &GossipHello_npc_altruis_the_sufferer;
     newscript->pGossipSelect = &GossipSelect_npc_altruis_the_sufferer;
     newscript->pQuestAccept =   &QuestAccept_npc_altruis_the_sufferer;
+    m_scripts[nrscripts++] = newscript;
+
+    newscript = new Script;
+    newscript->Name="npc_lantresor_of_the_blade";
+    newscript->pGossipHello = &GossipHello_npc_lantresor_of_the_blade;
+    newscript->pGossipSelect = &GossipSelect_npc_lantresor_of_the_blade;
     m_scripts[nrscripts++] = newscript;
 }
