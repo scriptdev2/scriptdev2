cf565c729fe069ef5bd749284f8fcc5b1b0be0cc
NoFantasy <nofantasy@nf.no>
2007-11-30 15:04:24 +0000

    [180] Added: more gossip/quest support regarding npc Altruis, story Illidan's Pupil, etc
    Added: more gossip/quest support regarding npc Altruis, story Illidan's Pupil, etc
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@180 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/zone/nagrand/npcs_nagrand.cpp
+++ scripts/zone/nagrand/npcs_nagrand.cpp
@@ -16,8 +16,8 @@
 
 /* ScriptData
 SDName: Npcs_Nagrand
-SD%Complete: 100
-SDComment: Nagrand NPCs
+SD%Complete: 85
+SDComment: Nagrand NPCs, Altruis gossip incomplete (but working) due to missing/unknown text id's
 EndScriptData */
 
 #include "../../sc_defines.h"
@@ -31,10 +31,82 @@ EndScriptData */
 ## npc_altruis_the_sufferer
 ######*/
 
+bool GossipHello_npc_altruis_the_sufferer(Player *player, Creature *_Creature)
+{
+    if (_Creature->isQuestGiver())
+        player->PrepareQuestMenu( _Creature->GetGUID() );
+
+    if (player->GetQuestRewardStatus(9982) == 1 || 
+        player->GetQuestRewardStatus(9983) == 1 && 
+        player->GetQuestRewardStatus(9991) == 0)
+        player->ADD_GOSSIP_ITEM( 0, "Tell me what you see.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+10);
+
+    if (player->GetQuestStatus(9991) == QUEST_STATUS_INCOMPLETE)
+        player->ADD_GOSSIP_ITEM( 0, "Well...?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+20);
+
+    if (player->GetQuestStatus(10646) == QUEST_STATUS_INCOMPLETE) //www.wowwiki.com/Varedis
+        player->ADD_GOSSIP_ITEM( 0, "[PH] Story about Illidan's Pupil", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+30);
+
+    player->PlayerTalkClass->SendGossipMenu(9419, _Creature->GetGUID());
+
+    return true;
+}
+
+bool GossipSelect_npc_altruis_the_sufferer(Player *player, Creature *_Creature, uint32 sender, uint32 action)
+{
+    switch (action)
+    {
+        case GOSSIP_ACTION_INFO_DEF+10:
+            player->ADD_GOSSIP_ITEM( 0, "Legion? What legion...", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 11);
+            player->SEND_GOSSIP_MENU(9420, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+11:
+            player->ADD_GOSSIP_ITEM( 0, "You can not see them clearely? Why is that?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 12);
+            player->SEND_GOSSIP_MENU(9421, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+12:
+            player->ADD_GOSSIP_ITEM( 0, "Yet you do not fear them?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 13);
+            player->SEND_GOSSIP_MENU(9422, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+13:
+            player->ADD_GOSSIP_ITEM( 0, "Forge camps?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 14);
+            player->SEND_GOSSIP_MENU(9423, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+14:
+            player->ADD_GOSSIP_ITEM( 0, "Sounds ok with me", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 15);
+            player->SEND_GOSSIP_MENU(9424, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+15:
+            player->PlayerTalkClass->CloseGossip();
+            break;
+
+        case GOSSIP_ACTION_INFO_DEF+20:
+            player->ADD_GOSSIP_ITEM( 0, "Ok.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 21);
+            player->SEND_GOSSIP_MENU(9427, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+21:
+            player->PlayerTalkClass->CloseGossip();
+            player->CompleteQuest(9991);
+            break;
+
+        case GOSSIP_ACTION_INFO_DEF+30:
+            player->ADD_GOSSIP_ITEM( 0, "[PH] Story done", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 31);
+            player->SEND_GOSSIP_MENU(384, _Creature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+31:
+            player->PlayerTalkClass->CloseGossip();
+            player->CompleteQuest(10646);
+            break;
+    }
+    return true;
+}
+
 bool QuestAccept_npc_altruis_the_sufferer(Player *player, Creature *creature, Quest const *quest )
 {
     if ( !player->GetQuestRewardStatus(9991) == 1 )         //Survey the Land, q-id 9991
     {
+        player->PlayerTalkClass->CloseGossip();
+
         std::vector<uint32> nodes;
 
         nodes.resize(2);
@@ -55,6 +127,8 @@ void AddSC_npcs_nagrand()
 
     newscript = new Script;
     newscript->Name="npc_altruis_the_sufferer";
+    newscript->pGossipHello = &GossipHello_npc_altruis_the_sufferer;
+    newscript->pGossipSelect = &GossipSelect_npc_altruis_the_sufferer;
     newscript->pQuestAccept =   &QuestAccept_npc_altruis_the_sufferer;
     m_scripts[nrscripts++] = newscript;
 }
