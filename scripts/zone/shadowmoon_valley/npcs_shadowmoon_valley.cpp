8d2ca77fc264231b0b32c1882a940fcc31938878
NoFantasy <nofantasy@nf.no>
2008-04-02 17:06:56 +0000

    [311] Added: Teleport trigger to Invasion Point: Cataclysm (DB support required for spell 37387&37389)
    Added: Teleport trigger to Invasion Point: Cataclysm (DB support required for spell 37387&37389)
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@311 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/zone/shadowmoon_valley/npcs_shadowmoon_valley.cpp
+++ scripts/zone/shadowmoon_valley/npcs_shadowmoon_valley.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: Npcs_Shadowmoon_Valley
 SD%Complete: 100
-SDComment: Vendor Drake Dealer Hurlunk.
+SDComment: Vendor Drake Dealer Hurlunk. Teleporter TO Invasion Point: Cataclysm
 SDCategory: Shadowmoon Valley
 EndScriptData */
 
@@ -49,6 +49,63 @@ bool GossipSelect_npc_drake_dealer_hurlunk(Player *player, Creature *_Creature, 
     return true;
 }
 
+/*######
+## npc_invis_legion_teleporter
+######*/
+
+struct MANGOS_DLL_DECL npc_invis_legion_teleporterAI : public ScriptedAI
+{
+    npc_invis_legion_teleporterAI(Creature *c) : ScriptedAI(c) {Reset();}
+
+    uint64 PlayerGuid;
+    uint32 TeleTimer;
+
+    void Reset()
+    {
+        PlayerGuid=0;
+        TeleTimer = 5000;
+    }
+
+    void MoveInLineOfSight(Unit *who)
+    {
+        if (!who || who->GetTypeId() != TYPEID_PLAYER)
+            return;
+
+        if(who->GetTypeId() == TYPEID_PLAYER && m_creature->GetDistance(who)<4)
+        {
+            if (who->isAlive() || !who->isInCombat())
+                PlayerGuid = who->GetGUID();
+        }
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(TeleTimer < diff)
+        {
+            if(PlayerGuid)
+            {
+                Player* player = ((Player*)Unit::GetUnit((*m_creature), PlayerGuid));
+                if(m_creature->GetDistance(player)<4)
+                {
+                    if(player->GetTeam()== ALLIANCE && player->GetQuestRewardStatus(10589))
+                        player->CastSpell(player,37387,false);
+                    if(player->GetTeam()== HORDE && player->GetQuestRewardStatus(10604))
+                        player->CastSpell(player,37389,false);
+                    //return? Cannot confirm this same npc should also be the one controlling return
+                    /*if(player->GetQuestRewardStatus(10589) || player->GetQuestRewardStatus(10604))
+                        player->CastSpell(player,37532,false);*/
+                }
+                PlayerGuid=0;
+            }
+            TeleTimer = 5000;
+        }else TeleTimer -= diff;
+    }
+};
+CreatureAI* GetAI_npc_invis_legion_teleporter(Creature *_Creature)
+{
+    return new npc_invis_legion_teleporterAI (_Creature);
+}
+
 void AddSC_npcs_shadowmoon_valley()
 {
     Script *newscript;
@@ -59,4 +116,8 @@ void AddSC_npcs_shadowmoon_valley()
     newscript->pGossipSelect = &GossipSelect_npc_drake_dealer_hurlunk;
     m_scripts[nrscripts++] = newscript;
 
+    newscript = new Script;
+    newscript->Name="npc_invis_legion_teleporter";
+    newscript->GetAI = GetAI_npc_invis_legion_teleporter;
+    m_scripts[nrscripts++] = newscript;
 }
--- sql/mangos_full_scripts.sql
+++ sql/mangos_full_scripts.sql
@@ -594,6 +594,7 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_zamael_lunthistle' WHERE `ent
 /* SHADOWMOON VALLEY */
 UPDATE `creature_template` SET `ScriptName` = 'neltharaku' WHERE `entry` = 21657;
 UPDATE `creature_template` SET `ScriptName` = 'npc_drake_dealer_hurlunk' WHERE `entry` = 23489;
+UPDATE `creature_template` SET `ScriptName` = 'npc_invis_legion_teleporter' WHERE `entry` = 21807;
 UPDATE `creature_template` SET `ScriptName` = 'mobs_dragonmaw_orc' WHERE `entry` IN (21717, 21718, 21719, 21720, 22331);
 UPDATE `creature_template` SET `ScriptName` = 'mobs_shadowmoon_valley_wildlife' WHERE `entry` IN (21878, 21879);
 /* Doomwalker */
