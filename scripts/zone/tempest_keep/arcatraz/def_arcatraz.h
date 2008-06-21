1e8202ef83a5153649c874692db716f77aae3e5f
NoFantasy <nofantasy@nf.no>
2008-06-21 11:41:26 +0000

    [452] Added: some placeholders/definitions for harbinger skyriss event(not implemented)
    Added: some placeholders/definitions for harbinger skyriss event(not implemented)
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@452 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/zone/tempest_keep/arcatraz/def_arcatraz.h
+++ scripts/zone/tempest_keep/arcatraz/def_arcatraz.h
@@ -4,8 +4,13 @@
 #include "sc_creature.h"
 #include "sc_instance.h"
 
-#define TYPE_ZEREKETH         1
-#define TYPE_DALLIAH          2
-#define TYPE_SOCCOTHRATES     3
-#define TYPE_HARBINGERSKYRISS 4
+#define TYPE_ZEREKETH           1
+#define TYPE_DALLIAH            2
+#define TYPE_SOCCOTHRATES       3
+#define TYPE_HARBINGERSKYRISS   4
+#define TYPE_WARDEN_1           5
+#define TYPE_WARDEN_2           6
+#define TYPE_WARDEN_3           7
+#define TYPE_WARDEN_4           8
+#define TYPE_WARDEN_5           9
 #endif
--- scripts/zone/tempest_keep/arcatraz/instance_arcatraz.cpp
+++ scripts/zone/tempest_keep/arcatraz/instance_arcatraz.cpp
@@ -24,16 +24,21 @@ EndScriptData */
 #include "def_arcatraz.h"
 #include "GameObject.h"
 
-#define ENCOUNTERS 4
+#define ENCOUNTERS 9
 
-#define CONTAINMENT_CORE_SECURITY_FIELD_ALPHA 184318 //door opened when Wrath-Scryer Soccothrates dies
-#define CONTAINMENT_CORE_SECURITY_FIELD_BETA  184319 //door opened when Dalliah the Doomsayer dies
+#define CONTAINMENT_CORE_SECURITY_FIELD_ALPHA 184318        //door opened when Wrath-Scryer Soccothrates dies
+#define CONTAINMENT_CORE_SECURITY_FIELD_BETA  184319        //door opened when Dalliah the Doomsayer dies
+#define POD_ALPHA   183961                                  //pod first boss wave
+#define POD_BETA    183963                                  //pod second boss wave
+#define POD_DELTA   183964                                  //pod third boss wave
+#define POD_GAMMA   183962                                  //pod fourth boss wave
+#define POD_OMEGA   183965                                  //pod fifth boss wave
 
 /* Arcatraz encounters:
 1 - Zereketh the Unbound event
 2 - Dalliah the Doomsayer event
 3 - Wrath-Scryer Soccothrates event
-4 - Harbinger Skyriss event
+4 - Harbinger Skyriss event, 5 sub-events
 */
 
 struct MANGOS_DLL_DECL instance_arcatraz : public ScriptedInstance
@@ -44,11 +49,21 @@ struct MANGOS_DLL_DECL instance_arcatraz : public ScriptedInstance
 
     GameObject *Containment_Core_Security_Field_Alpha;
     GameObject *Containment_Core_Security_Field_Beta;
+    GameObject *Pod_Alpha;
+    GameObject *Pod_Gamma;
+    GameObject *Pod_Beta;
+    GameObject *Pod_Delta;
+    GameObject *Pod_Omega;
 
     void Initialize()
     {
         Containment_Core_Security_Field_Alpha = NULL;
         Containment_Core_Security_Field_Beta  = NULL;
+        Pod_Alpha = NULL;
+        Pod_Beta  = NULL;
+        Pod_Delta = NULL;
+        Pod_Gamma = NULL;
+        Pod_Omega = NULL;
 
         for(uint8 i = 0; i < ENCOUNTERS; i++)
             Encounter[i] = false;
@@ -69,9 +84,23 @@ struct MANGOS_DLL_DECL instance_arcatraz : public ScriptedInstance
             case CONTAINMENT_CORE_SECURITY_FIELD_ALPHA:
                 Containment_Core_Security_Field_Alpha = go;
                 break;
-
             case CONTAINMENT_CORE_SECURITY_FIELD_BETA:
                 Containment_Core_Security_Field_Beta = go;
+                break;
+            case POD_ALPHA:
+                Pod_Alpha = go;
+                break;
+            case POD_BETA:
+                Pod_Beta = go;
+                break;
+            case POD_DELTA:
+                Pod_Delta = go;
+                break;
+            case POD_GAMMA:
+                Pod_Gamma = go;
+                break;
+            case POD_OMEGA:
+                Pod_Omega = go;
                 break;
         }
     }
@@ -113,6 +142,93 @@ struct MANGOS_DLL_DECL instance_arcatraz : public ScriptedInstance
             case TYPE_HARBINGERSKYRISS:
                 Encounter[3] = (data) ? true : false;
                 break;
+
+            case TYPE_WARDEN_1:
+            {
+                switch(data)
+                {
+                    case IN_PROGRESS:
+                        Encounter[4] = IN_PROGRESS;
+                        if(Pod_Alpha)
+                            Pod_Alpha->UseDoorOrButton();
+                        break;
+                    case DONE:
+                        Encounter[4] = DONE;
+                        break;
+                    default:
+                        Encounter[4] = (data) ? FAIL : NOT_STARTED;
+                        break;
+                }
+            }
+            case TYPE_WARDEN_2:
+            {
+                switch(data)
+                {
+                    case IN_PROGRESS:
+                        Encounter[5] = IN_PROGRESS;
+                        if(Pod_Beta)
+                            Pod_Beta->UseDoorOrButton();
+                        break;
+                    case DONE:
+                        Encounter[5] = DONE;
+                        break;
+                    default:
+                        Encounter[5] = (data) ? FAIL : NOT_STARTED;
+                        break;
+                }
+            }
+            case TYPE_WARDEN_3:
+            {
+                switch(data)
+                {
+                    case IN_PROGRESS:
+                        Encounter[6] = IN_PROGRESS;
+                        if(Pod_Delta)
+                            Pod_Delta->UseDoorOrButton();
+                        break;
+                    case DONE:
+                        Encounter[6] = DONE;
+                        break;
+                    default:
+                        Encounter[6] = (data) ? FAIL : NOT_STARTED;
+                        break;
+                }
+            }
+            case TYPE_WARDEN_4:
+            {
+                switch(data)
+                {
+                    case IN_PROGRESS:
+                        Encounter[7] = IN_PROGRESS;
+                        if(Pod_Gamma)
+                            Pod_Gamma->UseDoorOrButton();
+                        break;
+                    case DONE:
+                        Encounter[7] = DONE;
+                        break;
+                    default:
+                        Encounter[7] = (data) ? FAIL : NOT_STARTED;
+                        break;
+                }
+            }
+            case TYPE_WARDEN_5:
+            {
+                switch(data)
+                {
+                    case IN_PROGRESS:
+                        Encounter[8] = IN_PROGRESS;
+                        if(Pod_Omega)
+                            Pod_Omega->UseDoorOrButton();
+                        break;
+                    case DONE:
+                        Encounter[8] = DONE;
+                        break;
+                    default:
+                        Encounter[8] = (data) ? FAIL : NOT_STARTED;
+                        break;
+                }
+            }
+
         }
     }
 };
