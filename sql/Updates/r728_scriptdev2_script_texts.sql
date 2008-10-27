4e874fd7d8eb7c66a2b63e0129fbbcbbaf204955
NoFantasy <nofantasy@nf.no>
2008-10-27 16:54:07 +0000

    [728] Fixed: sql typo in previous commit
    Fixed: sql typo in previous commit
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@728 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ sql/Updates/r728_scriptdev2_script_texts.sql
@@ -0,0 +1,12 @@
+DELETE FROM `script_texts` WHERE `entry` BETWEEN -1509027 AND -1509018;
+INSERT INTO `script_texts` (`entry`,`content_default`,`sound`,`type`,`language`,`comment`) VALUES
+(-1509018,'I am rejuvinated!',8593,1,0,'ossirian SAY_SURPREME1'),
+(-1509019,'My powers are renewed!',8595,1,0,'ossirian SAY_SURPREME2'),
+(-1509020,'My powers return!',8596,1,0,'ossirian SAY_SURPREME3'),
+(-1509021,'Protect the city at all costs!',8597,1,0,'ossirian SAY_RAND_INTRO1'),
+(-1509022,'The walls have been breached!',8599,1,0,'ossirian SAY_RAND_INTRO2'),
+(-1509023,'To your posts. Defend the city.',8600,1,0,'ossirian SAY_RAND_INTRO3'),
+(-1509024,'Tresspassers will be terminated.',8601,1,0,'ossirian SAY_RAND_INTRO4'),
+(-1509025,'Sands of the desert rise and block out the sun!',8598,1,0,'ossirian SAY_AGGRO'),
+(-1509026,'You are terminated.',8602,1,0,'ossirian SAY_SLAY'),
+(-1509027,'I...have...failed.',8594,1,0,'ossirian SAY_DEATH');
