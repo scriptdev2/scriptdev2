8e52cfadc0be0974970bce64af28133bf57eb080
NoFantasy <nofantasy@nf.no>
2008-12-07 17:12:55 +0000

    [787] Added: placeholder files for black morass instance (for future development of this)
    Added: placeholder files for black morass instance (for future development of this)
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@787 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ scripts/zone/caverns_of_time/dark_portal/def_dark_portal.h
@@ -0,0 +1,8 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_DARKPORTAL_H
+#define DEF_DARKPORTAL_H
+
+#endif
