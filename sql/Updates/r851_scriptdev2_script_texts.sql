e50b568fcbe267e3500dcd3b1ed8cc10ae8e4857
NoFantasy <nofantasy@nf.no>
2009-01-22 18:50:44 +0000

    [851] Convert text to database for doomwalker + add missing to kazzak
    Convert text to database for doomwalker + add missing to kazzak
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@851 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ sql/Updates/r851_scriptdev2_script_texts.sql
@@ -0,0 +1,23 @@
+DELETE FROM script_texts WHERE entry BETWEEN -1000167 AND -1000147;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1000147,'I remember well the sting of defeat at the conclusion of the Third War. I have waited far too long for my revenge. Now the shadow of the Legion falls over this world. It is only a matter of time until all of your failed creation... is undone.',11332,1,0,0,'kazzak SAY_INTRO'),
+(-1000148,'The Legion will conquer all!',11333,1,0,0,'kazzak SAY_AGGRO1'),
+(-1000149,'All mortals will perish!',11334,1,0,0,'kazzak SAY_AGGRO2'),
+(-1000150,'All life must be eradicated!',11335,1,0,0,'kazzak SAY_SURPREME1'),
+(-1000151,'I\'ll rip the flesh from your bones!',11336,1,0,0,'kazzak SAY_SURPREME2'),
+(-1000152,'Kirel Narak!',11337,1,0,0,'kazzak SAY_KILL1'),
+(-1000153,'Contemptible wretch!',11338,1,0,0,'kazzak SAY_KILL2'),
+(-1000154,'The universe will be remade.',11339,1,0,0,'kazzak SAY_KILL3'),
+(-1000155,'The Legion... will never... fall.',11340,1,0,0,'kazzak SAY_DEATH'),
+(-1000156,'%s goes into a frenzy!',0,2,0,0,'kazzak EMOTE_FRENZY'),
+(-1000157,'Invaders, you dangle upon the precipice of oblivion! The Burning Legion comes and with it comes your end.',0,1,0,0,'kazzak SAY_RAND1'),
+(-1000158,'Impudent whelps, you only delay the inevitable. Where one has fallen, ten shall rise. Such is the will of Kazzak...',0,1,0,0,'kazzak SAY_RAND2'),
+(-1000159,'Do not proceed. You will be eliminated!',11344,1,0,0,'doomwalker SAY_AGGRO'),
+(-1000160,'Tectonic disruption commencing.',11345,1,0,0,'doomwalker SAY_EARTHQUAKE_1'),
+(-1000161,'Magnitude set. Release.',11346,1,0,0,'doomwalker SAY_EARTHQUAKE_2'),
+(-1000162,'Trajectory locked.',11347,1,0,0,'doomwalker SAY_OVERRUN_1'),
+(-1000163,'Engage maximum speed.',11348,1,0,0,'doomwalker SAY_OVERRUN_2'),
+(-1000164,'Threat level zero.',11349,1,0,0,'doomwalker SAY_SLAY_1'),
+(-1000165,'Directive accomplished.',11350,1,0,0,'doomwalker SAY_SLAY_2'),
+(-1000166,'Target exterminated.',11351,1,0,0,'doomwalker SAY_SLAY_3'),
+(-1000167,'System failure in five, f-o-u-r...',11352,1,0,0,'doomwalker SAY_DEATH');
