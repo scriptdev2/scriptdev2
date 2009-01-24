2cdf4b2cdabc387e00e43d659a291084ae98fe56
NoFantasy <nofantasy@nf.no>
2009-01-24 15:19:50 +0000

    [861] Remove obsolete source file.
    Remove obsolete source file.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@861 5f9c896b-1e26-0410-94da-f77f675e2462

--- ScriptMgr.cpp
+++ ScriptMgr.cpp
@@ -381,7 +381,6 @@ extern void AddSC_boss_anubrekhan();
 extern void AddSC_boss_four_horsemen();
 extern void AddSC_boss_faerlina();
 extern void AddSC_boss_gluth();
-extern void AddSC_boss_highlord_mograine();
 extern void AddSC_boss_kelthuzad();
 extern void AddSC_boss_loatheb();
 extern void AddSC_boss_maexxna();
@@ -1575,7 +1574,6 @@ void ScriptsInit()
     AddSC_boss_four_horsemen();
     AddSC_boss_faerlina();
     AddSC_boss_gluth();
-    AddSC_boss_highlord_mograine();
     AddSC_boss_kelthuzad();
     AddSC_boss_loatheb();
     AddSC_boss_maexxna();
