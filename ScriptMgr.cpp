9c5c505a9e80e01e9d8f0ca5137b5000fec11907
NoFantasy <nofantasy@nf.no>
2009-04-13 21:48:26 +0000

    [1003] Include data from sd2_db_version table in function used by .server info -command
    Include data from sd2_db_version table in function used by .server info -command
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1003 5f9c896b-1e26-0410-94da-f77f675e2462

--- ScriptMgr.cpp
+++ ScriptMgr.cpp
@@ -16,6 +16,8 @@ Script *m_scripts[MAX_SCRIPTS];
 DatabaseType SD2Database;
 Config SD2Config;
 
+std::string strVersion;
+
 // String text additional data, used in TextMap
 struct StringTextData
 {
@@ -593,16 +595,21 @@ void LoadDatabase()
     QueryResult *result;
 
     //Get Version information
-    result = SD2Database.PQuery("SELECT version FROM sd2_db_version");
+    result = SD2Database.PQuery("SELECT version FROM sd2_db_version LIMIT 1");
 
     if (result)
     {
         Field *fields = result->Fetch();
-        outstring_log("SD2: Database version is: %s", fields[0].GetString());
+
+        strVersion = fields[0].GetCppString();
+        strVersion.append(_FULLVERSION);
+
+        outstring_log("Loading %s", strVersion.c_str());
         outstring_log("");
         delete result;
 
-    }else
+    }
+    else
     {
         error_log("SD2: Missing `sd2_db_version` information.");
         outstring_log("");
@@ -665,7 +672,8 @@ void LoadDatabase()
 
         outstring_log("");
         outstring_log(">> Loaded %u additional Script Texts data.", count);
-    }else
+    }
+    else
     {
         barGoLink bar(1);
         bar.step();
@@ -730,7 +738,8 @@ void LoadDatabase()
 
         outstring_log("");
         outstring_log(">> Loaded %u additional Custom Texts data.", count);
-    }else
+    }
+    else
     {
         barGoLink bar(1);
         bar.step();
@@ -839,16 +848,14 @@ void ScriptsInit()
     outstring_log(" MMM  MMM  http://www.scriptdev2.com");
     outstring_log("");
 
-    outstring_log("ScriptDev2 initializing: %s", _FULLVERSION);
-    outstring_log("");
-
     //Get configuration file
     if (!SD2Config.SetSource(_SCRIPTDEV2_CONFIG))
     {
         CanLoadDB = false;
         error_log("SD2: Unable to open configuration file. Database will be unaccessible. Configuration values will use default.");
     }
-    else outstring_log("SD2: Using configuration file %s",_SCRIPTDEV2_CONFIG);
+    else
+        outstring_log("SD2: Using configuration file %s",_SCRIPTDEV2_CONFIG);
 
     //Check config file version
     if (SD2Config.GetIntDefault("ConfVersion", 0) != SD2_CONF_VERSION)
@@ -1509,7 +1516,9 @@ void Script::RegisterSelf()
 MANGOS_DLL_EXPORT
 char const* ScriptsVersion()
 {
-    //TODO: add version text from sd2_db_version
+    if (!strVersion.empty())
+        return strVersion.c_str();
+
     return _FULLVERSION;
 }
 
