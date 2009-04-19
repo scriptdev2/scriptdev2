231f40176144c77b89f92e688bdc8aca075d3e8f
NoFantasy <nofantasy@nf.no>
2009-04-19 11:54:25 +0000

    [1014] Remove old tables in SD2 database no longer used. Note this is for databases created before revision 986.
    Remove old tables in SD2 database no longer used. Note this is for databases created before revision 986.
    Remove ScriptName for script 'mob_eventai' that does no longer exist.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1014 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ sql/Updates/r1014_mangos.sql
@@ -0,0 +1,1 @@
+UPDATE creature_template SET ScriptName='' WHERE ScriptName='mob_eventai';
---
+++ sql/Updates/r1014_scriptdev2.sql
@@ -0,0 +1,5 @@
+-- Note: if SD2 installed after rev 986, you do not have to run this query (will produce error)
+-- This only intended for cleaning up older, existing SD2 database.
+DROP TABLE eventai_scripts;
+DROP TABLE eventai_summons;
+DROP TABLE eventai_texts;
