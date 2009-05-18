4310d2408d7783df5a30842f204f055486e42b62
NoFantasy <nofantasy@nf.no>
2009-05-18 12:29:53 +0000

    [1097] Remove obsolete functions redefined in 1094
    Remove obsolete functions redefined in 1094
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1097 5f9c896b-1e26-0410-94da-f77f675e2462

--- include/sc_grid_searchers.h
+++ include/sc_grid_searchers.h
@@ -19,9 +19,6 @@ Creature* GetClosestCreatureWithEntry(WorldObject* pSource, uint32 uiEntry, floa
 void GetGameObjectListWithEntryInGrid(std::list<GameObject*>& lList , WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
 void GetCreatureListWithEntryInGrid(std::list<Creature*>& lList, WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
 
-std::list<GameObject*> GetGameObjectListWithEntryInGrid(WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
-std::list<Creature*> GetCreatureListWithEntryInGrid(WorldObject* pSource, uint32 uiEntry, float fMaxSearchRange);
-
 //Used in:
 //hyjalAI.cpp
 class AllFriendlyCreaturesInGrid
