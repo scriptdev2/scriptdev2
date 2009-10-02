ba8028c3269483ae5edba8877b1f228d864aa650
NoFantasy <nofantasy@nf.no>
2009-10-02 00:20:01 +0000

    [1434] Implement phase 2 main ability for boss_onyxia.
    Implement phase 2 main ability for boss_onyxia.
    Note that spells require database support. See bottom of source file for data to spell_target_position db-table (may need smaller adjustments).
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1434 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/kalimdor/onyxias_lair/boss_onyxia.cpp
+++ scripts/kalimdor/onyxias_lair/boss_onyxia.cpp
@@ -16,8 +16,8 @@
 
 /* ScriptData
 SDName: Boss_Onyxia
-SD%Complete: 50
-SDComment: Phase 2 has many errors. Recommend Rewrite of entire script.
+SD%Complete: 65
+SDComment: Phase 3 need additianal code. Phase 2 requires entries in spell_target_position with specific locations. See bottom of file.
 SDCategory: Onyxia's Lair
 EndScriptData */
 
@@ -41,6 +41,20 @@ enum
     SPELL_DEEPBREATH            = 23461,
     SPELL_FIREBALL              = 18392,
 
+    //Not much choise about these. We have to make own defintion on the direction/start-end point
+    //SPELL_BREATH_NORTH_TO_SOUTH = 17086,                  // 20x in "array"
+    //SPELL_BREATH_SOUTH_TO_NORTH = 18351,                  // 11x in "array"
+
+    SPELL_BREATH_EAST_TO_WEST   = 18576,                    // 7x in "array"
+    SPELL_BREATH_WEST_TO_EAST   = 18609,                    // 7x in "array"
+
+    SPELL_BREATH_SE_TO_NW       = 18564,                    // 12x in "array"
+    SPELL_BREATH_NW_TO_SE       = 18584,                    // 12x in "array"
+    SPELL_BREATH_SW_TO_NE       = 18596,                    // 12x in "array"
+    SPELL_BREATH_NE_TO_SW       = 18617,                    // 12x in "array"
+
+    //SPELL_BREATH                = 21131,                  // 8x in "array", different initial cast than the other arrays
+
     SPELL_BELLOWINGROAR         = 18431,
     SPELL_HEATED_GROUND         = 22191,
 
@@ -53,15 +67,24 @@ enum
     PHASE_END                   = 3
 };
 
-static float afMovementLocations[7][3]=
+struct sOnyxMove
 {
-    {-65.8444, -213.809, -60.2985},
-    {22.87639, -217.152, -60.0548},
-    {-33.5561, -182.682, -60.9457},
-    {-31.4963, -250.123, -60.1278},
-    {-2.78999, -181.431, -60.8962},
-    {-54.9415, -232.242, -60.5555},
-    {10.56655, -241.478, -60.9426},
+    uint32 uiLocId;
+    uint32 uiLocIdEnd;
+    uint32 uiSpellId;
+    float fX, fY, fZ;
+};
+
+static sOnyxMove aMoveData[]=
+{
+    {0, 1, SPELL_BREATH_WEST_TO_EAST,   -33.5561f, -182.682f, -60.9457f},//west
+    {1, 0, SPELL_BREATH_EAST_TO_WEST,   -31.4963f, -250.123f, -60.1278f},//east
+    {2, 4, SPELL_BREATH_NW_TO_SE,         6.8951f, -180.246f, -60.896f},//north-west
+    {3, 5, SPELL_BREATH_NE_TO_SW,        10.2191f, -247.912f, -60.896f},//north-east
+    {4, 2, SPELL_BREATH_SE_TO_NW,       -63.5156f, -240.096f, -60.477f},//south-east
+    {5, 3, SPELL_BREATH_SW_TO_NE,       -58.2509f, -189.020f, -60.790f},//south-west
+    //{6, 7, SPELL_BREATH_SOUTH_TO_NORTH, -65.8444f, -213.809f, -60.2985f},//south
+    //{7, 6, SPELL_BREATH_NORTH_TO_SOUTH,  22.8763f, -217.152f, -60.0548f},//north
 };
 
 static float afSpawnLocations[2][3]=
@@ -81,7 +104,9 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
     uint32 m_uiTailSweepTimer;
     uint32 m_uiWingBuffetTimer;
 
+    uint32 m_uiMovePoint;
     uint32 m_uiMovementTimer;
+    sOnyxMove* m_pPointData;
 
     uint32 m_uiEngulfingFlamesTimer;
     uint32 m_uiSummonWhelpsTimer;
@@ -93,6 +118,9 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
 
     void Reset()
     {
+        if (!IsCombatMovement())
+            SetCombatMovement(true);
+
         m_uiPhase = PHASE_START;
 
         m_uiFlameBreathTimer = urand(10000, 20000);
@@ -100,7 +128,9 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
         m_uiCleaveTimer = urand(2000, 5000);
         m_uiWingBuffetTimer = urand(10000, 20000);
 
-        m_uiMovementTimer = 5000;
+        m_uiMovePoint = urand(0, 5);
+        m_uiMovementTimer = 20000;
+        m_pPointData = GetMoveData();
 
         m_uiEngulfingFlamesTimer = 15000;
         m_uiSummonWhelpsTimer = 45000;
@@ -128,6 +158,48 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
     void KilledUnit(Unit* pVictim)
     {
         DoScriptText(SAY_KILL, m_creature);
+    }
+
+    void SpellHit(Unit *pCaster, const SpellEntry* pSpell)
+    {
+        if (pSpell->Id == SPELL_BREATH_EAST_TO_WEST ||
+            pSpell->Id == SPELL_BREATH_WEST_TO_EAST ||
+            pSpell->Id == SPELL_BREATH_SE_TO_NW ||
+            pSpell->Id == SPELL_BREATH_NW_TO_SE ||
+            pSpell->Id == SPELL_BREATH_SW_TO_NE ||
+            pSpell->Id == SPELL_BREATH_NE_TO_SW)
+        {
+            if (m_pPointData)
+            {
+                m_creature->GetMap()->CreatureRelocation(m_creature, m_pPointData->fX, m_pPointData->fY, m_pPointData->fZ, 0.0f);
+                m_creature->SendMonsterMove(m_pPointData->fX, m_pPointData->fY, m_pPointData->fZ, 0, m_creature->GetMonsterMoveFlags(), 1);
+            }
+        }
+    }
+
+    sOnyxMove* GetMoveData()
+    {
+        uint32 uiMaxCount = sizeof(aMoveData)/sizeof(sOnyxMove);
+
+        for (uint32 i = 0; i < uiMaxCount; ++i)
+        {
+            if (aMoveData[i].uiLocId == m_uiMovePoint)
+                return &aMoveData[i];
+        }
+
+        return NULL;
+    }
+
+    void SetNextRandomPoint()
+    {
+        uint32 uiMaxCount = sizeof(aMoveData)/sizeof(sOnyxMove);
+
+        int iTemp = rand()%(uiMaxCount-1);
+
+        if (iTemp >= m_uiMovePoint)
+            ++iTemp;
+
+        m_uiMovePoint = iTemp;
     }
 
     void UpdateAI(const uint32 uiDiff)
@@ -192,7 +264,11 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
 
                     DoScriptText(SAY_PHASE_2_TRANS, m_creature);
 
-                    m_creature->HandleEmoteCommand(EMOTE_ONESHOT_LIFTOFF);
+                    if (m_pPointData)
+                        m_creature->GetMotionMaster()->MovePoint(m_pPointData->uiLocId, m_pPointData->fX, m_pPointData->fY, m_pPointData->fZ);
+
+                    SetNextRandomPoint();
+                    return;
                 }
             }
 
@@ -213,27 +289,40 @@ struct MANGOS_DLL_DECL boss_onyxiaAI : public ScriptedAI
 
             if (m_uiMovementTimer < uiDiff)
             {
-                uint32 uiRandom = urand(0, 7);
+                m_pPointData = GetMoveData();
 
-                if (uiRandom < 7)
-                    m_creature->GetMotionMaster()->MovePoint(0, afMovementLocations[uiRandom][0], afMovementLocations[uiRandom][1], afMovementLocations[uiRandom][2]);
-                else
-                {
-                    DoScriptText(EMOTE_BREATH, m_creature);
-                    DoCast(m_creature->getVictim(), SPELL_DEEPBREATH);
-                }
+                SetNextRandomPoint();
 
                 m_uiMovementTimer = 25000;
+
+                if (!m_pPointData)
+                    return;
+
+                if (m_uiMovePoint == m_pPointData->uiLocIdEnd)
+                {
+                    if (m_creature->IsNonMeleeSpellCasted(false))
+                        m_creature->InterruptNonMeleeSpells(false);
+
+                    DoScriptText(EMOTE_BREATH, m_creature);
+                    DoCast(m_creature, m_pPointData->uiSpellId);
+                }
+                else
+                {
+                    m_creature->GetMotionMaster()->MovePoint(m_pPointData->uiLocId, m_pPointData->fX, m_pPointData->fY, m_pPointData->fZ);
+                }
             }
             else
                 m_uiMovementTimer -= uiDiff;
 
             if (m_uiEngulfingFlamesTimer < uiDiff)
             {
-                if (Unit* pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(pTarget, SPELL_FIREBALL);
+                if (m_creature->GetMotionMaster()->GetCurrentMovementGeneratorType() != POINT_MOTION_TYPE)
+                {
+                    if (Unit* pTarget = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                        DoCast(pTarget, SPELL_FIREBALL);
 
-                m_uiEngulfingFlamesTimer = 8000;
+                    m_uiEngulfingFlamesTimer = 8000;
+                }
             }
             else
                 m_uiEngulfingFlamesTimer -= uiDiff;           //engulfingflames is supposed to be activated by a fireball but haven't come by
@@ -282,3 +371,117 @@ void AddSC_boss_onyxia()
     newscript->GetAI = &GetAI_boss_onyxiaAI;
     newscript->RegisterSelf();
 }
+
+/*
+-- SPELL_BREATH_EAST_TO_WEST
+DELETE FROM spell_target_position WHERE id IN (18576, 18578, 18579, 18580, 18581, 18582, 18583);
+INSERT INTO spell_target_position VALUES (18576, 249, -37.743851, -243.667923, -88.217651, 1.416);
+INSERT INTO spell_target_position VALUES (18578, 249, -35.805332, -232.028900, -87.749153, 1.416);
+INSERT INTO spell_target_position VALUES (18579, 249, -34.045738, -224.714661, -85.529465, 1.416);
+INSERT INTO spell_target_position VALUES (18580, 249, -32.081570, -214.916962, -88.327438, 1.416);
+INSERT INTO spell_target_position VALUES (18581, 249, -36.611721, -202.684677, -85.653786, 1.416);
+INSERT INTO spell_target_position VALUES (18582, 249, -37.067261, -195.758652, -87.745834, 1.416);
+INSERT INTO spell_target_position VALUES (18583, 249, -37.728523, -188.616806, -88.074898, 1.416);
+-- SPELL_BREATH_WEST_TO_EAST
+DELETE FROM spell_target_position WHERE id IN (18609, 18611, 18612, 18613, 18614, 18615, 18616);
+INSERT INTO spell_target_position VALUES (18609, 249, -37.728523, -188.616806, -88.074898, 4.526);
+INSERT INTO spell_target_position VALUES (18611, 249, -37.067261, -195.758652, -87.745834, 4.526);
+INSERT INTO spell_target_position VALUES (18612, 249, -36.611721, -202.684677, -85.653786, 4.526);
+INSERT INTO spell_target_position VALUES (18613, 249, -32.081570, -214.916962, -88.327438, 4.526);
+INSERT INTO spell_target_position VALUES (18614, 249, -34.045738, -224.714661, -85.529465, 4.526);
+INSERT INTO spell_target_position VALUES (18615, 249, -35.805332, -232.028900, -87.749153, 4.526);
+INSERT INTO spell_target_position VALUES (18616, 249, -37.743851, -243.667923, -88.217651, 4.526);
+-- SPELL_BREATH_NW_TO_SE
+DELETE FROM spell_target_position WHERE id IN (18584, 18585, 18586, 18587, 18588, 18589, 18590, 18591, 18592, 18593, 18594, 18595);
+INSERT INTO spell_target_position VALUES (18584, 249, 6.016711, -181.305771, -85.654648, 3.776);
+INSERT INTO spell_target_position VALUES (18585, 249, 3.860220, -183.227249, -86.375381, 3.776);
+INSERT INTO spell_target_position VALUES (18586, 249, -2.529650, -188.690491, -87.172859, 3.776);
+INSERT INTO spell_target_position VALUES (18587, 249, -8.449303, -193.957962, -87.564957, 3.776);
+INSERT INTO spell_target_position VALUES (18588, 249, -14.321238, -199.462219, -87.922478, 3.776);
+INSERT INTO spell_target_position VALUES (18589, 249, -15.602085, -216.893936, -88.403183, 3.776);
+INSERT INTO spell_target_position VALUES (18590, 249, -23.650263, -221.969086, -89.172699, 3.776);
+INSERT INTO spell_target_position VALUES (18591, 249, -29.495876, -213.014359, -88.910423, 3.776);
+INSERT INTO spell_target_position VALUES (18592, 249, -35.439922, -217.260284, -87.336311, 3.776);
+INSERT INTO spell_target_position VALUES (18593, 249, -41.762104, -221.896545, -86.114113, 3.776);
+INSERT INTO spell_target_position VALUES (18594, 249, -51.067528, -228.909988, -85.765556, 3.776);
+INSERT INTO spell_target_position VALUES (18595, 249, -56.559654, -241.223923, -85.423607, 3.776);
+-- SPELL_BREATH_SE_TO_NW
+DELETE FROM spell_target_position WHERE id IN (18564, 18565, 18566, 18567, 18568, 18569, 18570, 18571, 18572, 18573, 18574, 18575);
+INSERT INTO spell_target_position VALUES (18564, 249, -56.559654, -241.223923, -85.423607, 0.666);
+INSERT INTO spell_target_position VALUES (18565, 249, -51.067528, -228.909988, -85.765556, 0.666);
+INSERT INTO spell_target_position VALUES (18566, 249, -41.762104, -221.896545, -86.114113, 0.666);
+INSERT INTO spell_target_position VALUES (18567, 249, -35.439922, -217.260284, -87.336311, 0.666);
+INSERT INTO spell_target_position VALUES (18568, 249, -29.495876, -213.014359, -88.910423, 0.666);
+INSERT INTO spell_target_position VALUES (18569, 249, -23.650263, -221.969086, -89.172699, 0.666);
+INSERT INTO spell_target_position VALUES (18570, 249, -15.602085, -216.893936, -88.403183, 0.666);
+INSERT INTO spell_target_position VALUES (18571, 249, -14.321238, -199.462219, -87.922478, 0.666);
+INSERT INTO spell_target_position VALUES (18572, 249, -8.449303, -193.957962, -87.564957, 0.666);
+INSERT INTO spell_target_position VALUES (18573, 249, -2.529650, -188.690491, -87.172859, 0.666);
+INSERT INTO spell_target_position VALUES (18574, 249, 3.860220, -183.227249, -86.375381, 0.666);
+INSERT INTO spell_target_position VALUES (18575, 249, 6.016711, -181.305771, -85.654648, 0.666);
+-- SPELL_BREATH_SW_TO_NE
+DELETE FROM spell_target_position WHERE id IN (18596, 18597, 18598, 18599, 18600, 18601, 18602, 18603, 18604, 18605, 18606, 18607);
+INSERT INTO spell_target_position VALUES (18596, 249, -58.250900, -189.020004, -85.292267, 5.587);
+INSERT INTO spell_target_position VALUES (18597, 249, -52.006271, -193.796570, -85.808769, 5.587);
+INSERT INTO spell_target_position VALUES (18598, 249, -46.135464, -198.548553, -85.901764, 5.587);
+INSERT INTO spell_target_position VALUES (18599, 249, -40.500187, -203.001053, -85.555107, 5.587);
+INSERT INTO spell_target_position VALUES (18600, 249, -30.907579, -211.058197, -88.592125, 5.587);
+INSERT INTO spell_target_position VALUES (18601, 249, -20.098139, -218.681427, -88.937088, 5.587);
+INSERT INTO spell_target_position VALUES (18602, 249, -12.223192, -224.666168, -87.856300, 5.587);
+INSERT INTO spell_target_position VALUES (18603, 249, -6.475297, -229.098724, -87.076401, 5.587);
+INSERT INTO spell_target_position VALUES (18604, 249, -2.010256, -232.541992, -86.995140, 5.587);
+INSERT INTO spell_target_position VALUES (18605, 249, 2.736300, -236.202347, -86.790367, 5.587);
+INSERT INTO spell_target_position VALUES (18606, 249, 7.197779, -239.642868, -86.307297, 5.587);
+INSERT INTO spell_target_position VALUES (18607, 249, 12.120926, -243.439407, -85.874260, 5.587);
+-- SPELL_BREATH_NE_TO_SW
+DELETE FROM spell_target_position WHERE id IN (18617, 18619, 18620, 18621, 18622, 18623, 18624, 18625, 18626, 18627, 18628, 18618);
+INSERT INTO spell_target_position VALUES (18617, 249, 12.120926, -243.439407, -85.874260, 2.428);
+INSERT INTO spell_target_position VALUES (18619, 249, 7.197779, -239.642868, -86.307297, 2.428);
+INSERT INTO spell_target_position VALUES (18620, 249, 2.736300, -236.202347, -86.790367, 2.428);
+INSERT INTO spell_target_position VALUES (18621, 249, -2.010256, -232.541992, -86.995140, 2.428);
+INSERT INTO spell_target_position VALUES (18622, 249, -6.475297, -229.098724, -87.076401, 2.428);
+INSERT INTO spell_target_position VALUES (18623, 249, -12.223192, -224.666168, -87.856300, 2.428);
+INSERT INTO spell_target_position VALUES (18624, 249, -20.098139, -218.681427, -88.937088, 2.428);
+INSERT INTO spell_target_position VALUES (18625, 249, -30.907579, -211.058197, -88.592125, 2.428);
+INSERT INTO spell_target_position VALUES (18626, 249, -40.500187, -203.001053, -85.555107, 2.428);
+INSERT INTO spell_target_position VALUES (18627, 249, -46.135464, -198.548553, -85.901764, 2.428);
+INSERT INTO spell_target_position VALUES (18628, 249, -52.006271, -193.796570, -85.808769, 2.428);
+INSERT INTO spell_target_position VALUES (18618, 249, -58.250900, -189.020004, -85.292267, 2.428);
+
+-- Below is not needed for current script
+-- SPELL_BREATH_SOUTH_TO_NORTH
+DELETE FROM spell_target_position WHERE id IN (18351, 18352, 18353, 18354, 18355, 18356, 18357, 18358, 18359, 18360, 18361);
+INSERT INTO spell_target_position VALUES (18351, 249, -68.834236, -215.036163, -84.018875, 6.280);
+INSERT INTO spell_target_position VALUES (18352, 249, -61.834255, -215.051910, -84.673416, 6.280);
+INSERT INTO spell_target_position VALUES (18353, 249, -53.343277, -215.071014, -85.597191, 6.280);
+INSERT INTO spell_target_position VALUES (18354, 249, -42.619305, -215.095139, -86.663605, 6.280);
+INSERT INTO spell_target_position VALUES (18355, 249, -35.899323, -215.110245, -87.196548, 6.280);
+INSERT INTO spell_target_position VALUES (18356, 249, -28.248341, -215.127457, -89.191750, 6.280);
+INSERT INTO spell_target_position VALUES (18357, 249, -20.324360, -215.145279, -88.963997, 6.280);
+INSERT INTO spell_target_position VALUES (18358, 249, -11.189384, -215.165833, -87.817093, 6.280);
+INSERT INTO spell_target_position VALUES (18359, 249, -2.047405, -215.186386, -86.279655, 6.280);
+INSERT INTO spell_target_position VALUES (18360, 249, 7.479571, -215.207809, -86.075531, 6.280);
+INSERT INTO spell_target_position VALUES (18361, 249, 20.730539, -215.237610, -85.254387, 6.280);
+-- SPELL_BREATH_NORTH_TO_SOUTH
+DELETE FROM spell_target_position WHERE id IN (17086, 17087, 17088, 17089, 17090, 17091, 17092, 17093, 17094, 17095, 17097, 22267, 22268, 21132, 21133, 21135, 21136, 21137, 21138, 21139);
+INSERT INTO spell_target_position VALUES (17086, 249, 20.730539, -215.237610, -85.254387, 3.142);
+INSERT INTO spell_target_position VALUES (17087, 249, 7.479571, -215.207809, -86.075531, 3.142);
+INSERT INTO spell_target_position VALUES (17088, 249, -2.047405, -215.186386, -86.279655, 3.142);
+INSERT INTO spell_target_position VALUES (17089, 249, -11.189384, -215.165833, -87.817093, 3.142);
+INSERT INTO spell_target_position VALUES (17090, 249, -20.324360, -215.145279, -88.963997, 3.142);
+INSERT INTO spell_target_position VALUES (17091, 249, -28.248341, -215.127457, -89.191750, 3.142);
+INSERT INTO spell_target_position VALUES (17092, 249, -35.899323, -215.110245, -87.196548, 3.142);
+INSERT INTO spell_target_position VALUES (17093, 249, -42.619305, -215.095139, -86.663605, 3.142);
+INSERT INTO spell_target_position VALUES (17094, 249, -53.343277, -215.071014, -85.597191, 3.142);
+INSERT INTO spell_target_position VALUES (17095, 249, -61.834255, -215.051910, -84.673416, 3.142);
+INSERT INTO spell_target_position VALUES (17097, 249, -68.834236, -215.036163, -84.018875, 3.142);
+INSERT INTO spell_target_position VALUES (22267, 249, -75.736046, -214.984970, -83.394188, 3.142);
+INSERT INTO spell_target_position VALUES (22268, 249, -84.087578, -214.857834, -82.640053, 3.142);
+INSERT INTO spell_target_position VALUES (21132, 249, -90.424416, -214.601974, -82.482697, 3.142);
+INSERT INTO spell_target_position VALUES (21133, 249, -96.572411, -214.353745, -82.239967, 3.142);
+INSERT INTO spell_target_position VALUES (21135, 249, -102.069931, -214.131775, -80.571190, 3.142);
+INSERT INTO spell_target_position VALUES (21136, 249, -107.385597, -213.917145, -77.447037, 3.142);
+INSERT INTO spell_target_position VALUES (21137, 249, -114.281258, -213.866486, -73.851128, 3.142);
+INSERT INTO spell_target_position VALUES (21138, 249, -123.328560, -213.607910, -71.559921, 3.142);
+INSERT INTO spell_target_position VALUES (21139, 249, -130.788300, -213.424026, -70.751007, 3.142);
+*/
