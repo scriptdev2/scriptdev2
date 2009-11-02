b571520b32a9ed0ac177fc6c0d20aa835e6461d3
Reve <reve@scriptdev2.com>
2009-11-02 09:52:27 +0000

    [1493] Updated MaNGOS patch file. Requires MaNGOS 8759+
    Updated MaNGOS patch file. Requires MaNGOS 8759+
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1493 5f9c896b-1e26-0410-94da-f77f675e2462

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOS 8758+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOS 8759+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
