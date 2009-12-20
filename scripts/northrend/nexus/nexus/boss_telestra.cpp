6c8f1c5c1a310001522bdae6b277257dc5e23830
NoFantasy <nofantasy@nf.no>
2009-12-20 17:33:40 +0000

    [1522] Add phase 1 abilities for boss_telestra
    Add phase 1 abilities for boss_telestra
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1522 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/northrend/nexus/nexus/boss_telestra.cpp
+++ scripts/northrend/nexus/nexus/boss_telestra.cpp
@@ -16,8 +16,8 @@
 
 /* ScriptData
 SDName: Boss_Telestra
-SD%Complete: 10%
-SDComment:
+SD%Complete: 30%
+SDComment: phase 2 and 3 todo
 SDCategory: Nexus
 EndScriptData */
 
@@ -59,7 +59,11 @@ enum
 
     NPC_TELEST_FIRE         = 26928,
     NPC_TELEST_ARCANE       = 26929,
-    NPC_TELEST_FROST        = 26930
+    NPC_TELEST_FROST        = 26930,
+
+    PHASE_1                 = 1,
+    PHASE_2                 = 2,
+    PHASE_3                 = 3
 };
 
 /*######
@@ -78,8 +82,31 @@ struct MANGOS_DLL_DECL boss_telestraAI : public ScriptedAI
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
+    uint8 m_uiPhase;
+
+    uint32 m_uiFirebombTimer;
+    uint32 m_uiIceNovaTimer;
+    uint32 m_uiGravityWellTimer;
+
     void Reset()
     {
+        m_uiPhase = PHASE_1;
+
+        m_uiFirebombTimer = urand(3000, 5000);
+        m_uiIceNovaTimer = urand(6000, 12000);
+        m_uiGravityWellTimer = urand(15000, 25000);
+    }
+
+    void AttackStart(Unit* pWho)
+    {
+        if (m_creature->Attack(pWho, true))
+        {
+            m_creature->AddThreat(pWho);
+            m_creature->SetInCombatWith(pWho);
+            pWho->SetInCombatWith(m_creature);
+
+            m_creature->GetMotionMaster()->MoveChase(pWho, 20.0f);
+        }
     }
 
     void Aggro(Unit* pWho)
@@ -106,7 +133,56 @@ struct MANGOS_DLL_DECL boss_telestraAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        DoMeleeAttackIfReady();
+        switch(m_uiPhase)
+        {
+            case PHASE_1:
+            {
+                if (m_uiGravityWellTimer < uiDiff)
+                {
+                    if (!m_creature->IsNonMeleeSpellCasted(false))
+                    {
+                        DoCast(m_creature, SPELL_GRAVITY_WELL);
+                        m_uiGravityWellTimer = urand(15000, 30000);
+                    }
+                }
+                else
+                    m_uiGravityWellTimer -= uiDiff;
+
+                if (m_uiFirebombTimer < uiDiff)
+                {
+                    if (!m_creature->IsNonMeleeSpellCasted(false))
+                    {
+                        DoCast(m_creature->getVictim(), m_bIsRegularMode ? SPELL_FIREBOMB : SPELL_FIREBOMB_H);
+                        m_uiFirebombTimer = urand(3000, 5000);
+                    }
+                }
+                else
+                    m_uiFirebombTimer -= uiDiff;
+
+                if (m_uiIceNovaTimer < uiDiff)
+                {
+                    if (!m_creature->IsNonMeleeSpellCasted(false))
+                    {
+                        DoCast(m_creature, m_bIsRegularMode ? SPELL_ICE_NOVA : SPELL_ICE_NOVA_H);
+                        m_uiIceNovaTimer = urand(10000, 15000);
+                    }
+                }
+                else
+                    m_uiIceNovaTimer -= uiDiff;
+
+                DoMeleeAttackIfReady();
+                break;
+            }
+            case PHASE_2:
+            {
+                break;
+            }
+            case PHASE_3:
+            {
+                DoMeleeAttackIfReady();
+                break;
+            }
+        }
     }
 };
 
