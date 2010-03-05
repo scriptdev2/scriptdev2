1d1d63c5099618421068f2b7ffe7f3d2f2e17167
Reve <reve@scriptdev2.com>
2010-03-05 23:21:47 +0000

    [1633] Added one missing file from previous commit.
    Added one missing file from previous commit.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1633 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ scripts/northrend/crusaders_coliseum/trial_of_the_crusader/boss_jaraxxus.cpp
@@ -0,0 +1,82 @@
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: trial_of_the_crusader
+SD%Complete: 0
+SDComment:
+SDCategory: Crusader Coliseum
+EndScriptData */
+
+#include "precompiled.h"
+#include "trial_of_the_crusader.h"
+
+/*######
+## boss_jaraxxus
+######*/
+
+struct MANGOS_DLL_DECL boss_jaraxxusAI : public ScriptedAI
+{
+    boss_jaraxxusAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    ScriptedInstance* m_pInstance;
+
+    void Reset() {}
+
+    void JustReachedHome()
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_JARAXXUS, NOT_STARTED);
+    }
+
+    void JustDied(Unit* pKiller)
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_JARAXXUS, DONE);
+    }
+
+    void Aggro(Unit* pWho)
+    {
+        m_creature->SetInCombatWithZone();
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_boss_jaraxxus(Creature* pCreature)
+{
+    return new boss_jaraxxusAI(pCreature);
+}
+
+void AddSC_boss_jaraxxus()
+{
+    Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "boss_jaraxxus";
+    newscript->GetAI = &GetAI_boss_jaraxxus;
+    newscript->RegisterSelf();
+}
