0da066fa708a91451774c3e5292ec6b109bbc206
Reve <reve@scriptdev2.com>
2010-05-10 18:29:18 +0000

    [1686] Rewritten the Sunken Temple Instance Script.
    Rewritten the Sunken Temple Instance Script.
    Added support for opening door before Jammal'an the Prophet.
    ACID support needed for proper work.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1686 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/eastern_kingdoms/sunken_temple/instance_sunken_temple.cpp
+++ scripts/eastern_kingdoms/sunken_temple/instance_sunken_temple.cpp
@@ -16,7 +16,7 @@
 
 /* ScriptData
 SDName: instance_sunken_temple
-SD%Complete: 20
+SD%Complete: 30
 SDComment:
 SDCategory: Sunken Temple
 EndScriptData */
@@ -24,95 +24,124 @@ EndScriptData */
 #include "precompiled.h"
 #include "sunken_temple.h"
 
-struct MANGOS_DLL_DECL instance_sunken_temple : public ScriptedInstance
+instance_sunken_temple::instance_sunken_temple(Map* pMap) : ScriptedInstance(pMap),
+    m_uiJammalainBarrierGUID(0),
+    m_uiProtectorsRemaining(0)
 {
-    instance_sunken_temple(Map* pMap) : ScriptedInstance(pMap) {Initialize();};
+    Initialize();
+}
 
-    uint32 m_auiEncounter[MAX_ENCOUNTER];
-    std::string strInstData;
+void instance_sunken_temple::Initialize()
+{
+    memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+}
 
-    void Initialize()
+void instance_sunken_temple::OnObjectCreate(GameObject* pGo)
+{
+    switch(pGo->GetEntry())
     {
-        memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+        case GO_JAMMALAIN_BARRIER:
+            m_uiJammalainBarrierGUID = pGo->GetGUID();
+            if (m_auiEncounter[1] == DONE)
+                DoUseDoorOrButton(m_uiJammalainBarrierGUID);
+            break;
     }
 
-    void SetData(uint32 uiType, uint32 uiData)
+}
+
+void instance_sunken_temple::OnCreatureCreate(Creature* pCreature)
+{
+    switch(pCreature->GetEntry())
     {
-        debug_log("SD2: Instance Sunken Temple: SetData received for type %u with data %u", uiType, uiData);
+        case NPC_ZOLO:
+        case NPC_GASHER:
+        case NPC_LORO:
+        case NPC_HUKKU:
+        case NPC_ZULLOR:
+        case NPC_MIJAN:
+            ++m_uiProtectorsRemaining;
+            break;
+    }
+}
 
-        switch(uiType)
-        {
-            case TYPE_ATALARION:
-                m_auiEncounter[0] = uiData;
-                break;
-            case TYPE_DEFENDERS:
-                m_auiEncounter[1] = uiData;
-                break;
-            case TYPE_JAMMALAIN:
-                m_auiEncounter[2] = uiData;
-                break;
-            case TYPE_MALFURION:
-                m_auiEncounter[3] = uiData;
-                break;
-        }
-
-        if (uiData == DONE)
-        {
-            OUT_SAVE_INST_DATA;
-
-            std::ostringstream saveStream;
-            saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " " << m_auiEncounter[3];
-
-            strInstData = saveStream.str();
-
-            SaveToDB();
-            OUT_SAVE_INST_DATA_COMPLETE;
-        }
+void instance_sunken_temple::SetData(uint32 uiType, uint32 uiData)
+{
+    switch(uiType)
+    {
+        case TYPE_ATALARION:
+            m_auiEncounter[0] = uiData;
+            break;
+        case TYPE_PROTECTORS:
+            if (uiData == DONE)
+            {
+                //Jammalain should yell here about barrier being destroyed
+                --m_uiProtectorsRemaining;
+                if (!m_uiProtectorsRemaining)
+                {
+                    m_auiEncounter[1] = uiData;
+                    DoUseDoorOrButton(m_uiJammalainBarrierGUID);
+                }
+            }
+            break;
+        case TYPE_JAMMALAIN:
+            m_auiEncounter[2] = uiData;
+            break;
+        case TYPE_MALFURION:
+            m_auiEncounter[3] = uiData;
+            break;
     }
 
-    uint32 GetData(uint32 uiType)
+    if (uiData == DONE)
     {
-        switch(uiType)
-        {
-            case TYPE_ATALARION:
-                return m_auiEncounter[0];
-            case TYPE_DEFENDERS:
-                return m_auiEncounter[1];
-            case TYPE_JAMMALAIN:
-                return m_auiEncounter[2];
-            case TYPE_MALFURION:
-                return m_auiEncounter[3];
-        }
-        return 0;
+        OUT_SAVE_INST_DATA;
+
+        std::ostringstream saveStream;
+
+        strInstData = saveStream.str();
+        saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " " << m_auiEncounter[3];
+
+        SaveToDB();
+        OUT_SAVE_INST_DATA_COMPLETE;
+    }
+}
+
+void instance_sunken_temple::Load(const char* chrIn)
+{
+    if (!chrIn)
+    {
+        OUT_LOAD_INST_DATA_FAIL;
+        return;
     }
 
-    const char* Save()
+    OUT_LOAD_INST_DATA(chrIn);
+
+    std::istringstream loadStream(chrIn);
+    loadStream >> m_auiEncounter[0] >> m_auiEncounter[1] >> m_auiEncounter[2] >> m_auiEncounter[3];
+
+    for(uint8 i = 0; i < MAX_ENCOUNTER; ++i)
     {
-        return strInstData.c_str();
+        if (m_auiEncounter[i] == IN_PROGRESS)
+            m_auiEncounter[i] = NOT_STARTED;
     }
 
-    void Load(const char* chrIn)
+    OUT_LOAD_INST_DATA_COMPLETE;
+}
+
+uint32 instance_sunken_temple::GetData(uint32 uiType)
+{
+    switch(uiType)
     {
-        if (!chrIn)
-        {
-            OUT_LOAD_INST_DATA_FAIL;
-            return;
-        }
-
-        OUT_LOAD_INST_DATA(chrIn);
-
-        std::istringstream loadStream(chrIn);
-        loadStream >> m_auiEncounter[0] >> m_auiEncounter[1] >> m_auiEncounter[2] >> m_auiEncounter[3];
-
-        for(uint8 i = 0; i < MAX_ENCOUNTER; ++i)
-        {
-            if (m_auiEncounter[i] == IN_PROGRESS)
-                m_auiEncounter[i] = NOT_STARTED;
-        }
-
-        OUT_LOAD_INST_DATA_COMPLETE;
+        case TYPE_ATALARION:
+            return m_auiEncounter[0];
+        case TYPE_PROTECTORS:
+            return m_auiEncounter[1];
+        case TYPE_JAMMALAIN:
+            return m_auiEncounter[2];
+        case TYPE_MALFURION:
+            return m_auiEncounter[3];
     }
-};
+    return 0;
+}
 
 InstanceData* GetInstanceData_instance_sunken_temple(Map* pMap)
 {
--- scripts/eastern_kingdoms/sunken_temple/sunken_temple.h
+++ scripts/eastern_kingdoms/sunken_temple/sunken_temple.h
@@ -2,12 +2,15 @@
  * This program is free software licensed under GPL version 2
  * Please see the included DOCS/LICENSE.TXT for more information */
 
+#ifndef DEF_SUNKEN_TEMPLE_H
+#define DEF_SUNKEN_TEMPLE_H
+
 enum
 {
     MAX_ENCOUNTER         = 4,
 
     TYPE_ATALARION        = 1,
-    TYPE_DEFENDERS        = 2,
+    TYPE_PROTECTORS       = 2,
     TYPE_JAMMALAIN        = 3,
     TYPE_MALFURION        = 4,
 
@@ -16,6 +19,14 @@ enum
     NPC_WEAVER            = 5720,
     NPC_AVATAR_OF_HAKKAR  = 8443,
     NPC_SHADE_OF_ERANIKUS = 5709,
+
+    // Jammalain min-bosses
+    NPC_ZOLO              = 5712,
+    NPC_GASHER            = 5713,
+    NPC_LORO              = 5714,
+    NPC_HUKKU             = 5715,
+    NPC_ZULLOR            = 5716,
+    NPC_MIJAN             = 5717,
 
     NPC_MALFURION         = 15362,
 
@@ -26,5 +37,38 @@ enum
     GO_ATALAI_STATUE_3    = 148832,
     GO_ATALAI_STATUE_4    = 148833,
     GO_ATALAI_STATUE_5    = 148834,
-    GO_ATALAI_STATUE_6    = 148835
+    GO_ATALAI_STATUE_6    = 148835,
+
+    GO_ETERNAL_FLAME_1    = 148418,
+    GO_ETERNAL_FLAME_2    = 148419,
+    GO_ETERNAL_FLAME_3    = 148420,
+    GO_ETERNAL_FLAME_4    = 148421,
+
+    GO_JAMMALAIN_BARRIER  = 149431
 };
+
+class MANGOS_DLL_DECL instance_sunken_temple : public ScriptedInstance
+{
+    public:
+        instance_sunken_temple(Map* pMap);
+        ~instance_sunken_temple() {}
+
+        void Initialize();
+
+        void OnObjectCreate(GameObject* pGo);
+        void OnCreatureCreate(Creature* pCreature);
+
+        void SetData(uint32 uiType, uint32 uiData);
+        uint32 GetData(uint32 uiType);
+
+        const char* Save() { return strInstData.c_str(); }
+        void Load(const char* chrIn);
+
+    protected:
+        uint32 m_auiEncounter[MAX_ENCOUNTER];
+        std::string strInstData;
+
+        uint64 m_uiJammalainBarrierGUID;
+        uint8 m_uiProtectorsRemaining;
+};
+#endif
