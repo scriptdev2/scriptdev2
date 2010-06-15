cf3a77a0b84dfb365b0ae8ac008c5665c9b9292c
Reve <reve@scriptdev2.com>
2010-06-15 18:11:03 +0000

    [1714] Added basic support for boss Jedoga Shadowseeker.
    Added basic support for boss Jedoga Shadowseeker.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1714 5f9c896b-1e26-0410-94da-f77f675e2462

--- scripts/northrend/azjol-nerub/ahnkahet/boss_jedoga.cpp
+++ scripts/northrend/azjol-nerub/ahnkahet/boss_jedoga.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,32 +16,50 @@
 
 /* ScriptData
 SDName: Boss_Jedoga
-SD%Complete: 20%
-SDComment:
+SD%Complete: 40
+SDComment: 
 SDCategory: Ahn'kahet
 EndScriptData */
 
 #include "precompiled.h"
+#include "ahnkahet.h"
 
 enum
 {
-    SAY_AGGRO                           = -1619017,
-    SAY_CALL_SACRIFICE_1                = -1619018,
-    SAY_CALL_SACRIFICE_2                = -1619019,
-    SAY_SACRIFICE_1                     = -1619020,
-    SAY_SACRIFICE_2                     = -1619021,
-    SAY_SLAY_1                          = -1619022,
-    SAY_SLAY_2                          = -1619023,
-    SAY_SLAY_3                          = -1619024,
-    SAY_DEATH                           = -1619025,
-    SAY_PREACHING_1                     = -1619026,
-    SAY_PREACHING_2                     = -1619027,
-    SAY_PREACHING_3                     = -1619028,
-    SAY_PREACHING_4                     = -1619029,
-    SAY_PREACHING_5                     = -1619030,
+    SAY_AGGRO                = -1619017,
+    SAY_CALL_SACRIFICE1      = -1619018,
+    SAY_CALL_SACRIFICE2      = -1619019,
+    SAY_SACRIFICE1           = -1619020,
+    SAY_SACRIFICE2           = -1619021,
+    SAY_SLAY_1               = -1619022,
+    SAY_SLAY_2               = -1619023,
+    SAY_SLAY_3               = -1619024,
+    SAY_DEATH                = -1619025,
 
-    SAY_VOLUNTEER_1                     = -1619031,         //said by the volunteer image
-    SAY_VOLUNTEER_2                     = -1619032
+    // preaching 1-5 when it is used?
+    SAY_PREACHING1           = -1619026,
+    SAY_PREACHING2           = -1619027,
+    SAY_PREACHING3           = -1619028,
+    SAY_PREACHING4           = -1619029,
+    SAY_PREACHING5           = -1619030,
+
+    SAY_VOLUNTEER_CHOOSEN    = -1619031,                    // I have been choosen!
+    SAY_VOLUNTEER_SACRIFICED = -1619032,                    // I give myself to the master!
+
+    SPELL_CYCLONE_STRIKE     = 56855,
+    SPELL_CYCLONE_STRIKE_H   = 60030,
+    SPELL_LIGHTNING_BOLT     = 56891,
+    SPELL_LIGHTNING_BOLT_H   = 60032,
+    SPELL_THUNDERSHOCK       = 56926,
+    SPELL_THUNDERSHOCK_H     = 60029,
+    SPELL_GIFT_OF_THE_HERALD = 56219,
+
+    SPELL_SACRIFICE_VISUAL   = 56133,
+    SPELL_SACRIFICE_BEAM     = 56150,
+
+    NPC_JEDOGA_CONTROLLER    = 30181,
+    NPC_TWILIGHT_INITIATE    = 30114,
+    NPC_TWILIGHT_VOLUNTEER   = 30385
 };
 
 /*######
@@ -60,8 +78,32 @@ struct MANGOS_DLL_DECL boss_jedogaAI : public ScriptedAI
     ScriptedInstance* m_pInstance;
     bool m_bIsRegularMode;
 
+    uint32 m_uiThundershockTimer;
+    uint32 m_uiCycloneStrikeTimer;
+    uint32 m_uiLightningBoltTimer;
+    uint8 m_uiSacrificeCount;
+    bool m_bSacrifice;
+
     void Reset()
     {
+        m_uiThundershockTimer  = 40000;
+        m_uiCycloneStrikeTimer = 15000;
+        m_uiLightningBoltTimer = 7000;
+        m_bSacrifice           = false;
+    }
+
+    Creature* SelectRandomCreatureOfEntryInRange(uint32 uiEntry, float fRange)
+    {
+        std::list<Creature* > lCreatureList;
+        GetCreatureListWithEntryInGrid(lCreatureList, m_creature, uiEntry, fRange);
+
+        if (lCreatureList.empty())
+            return NULL;
+
+        std::list<Creature* >::iterator iter = lCreatureList.begin();
+        advance(iter, urand(0, lCreatureList.size()-1));
+
+        return *iter;
     }
 
     void Aggro(Unit* pWho)
@@ -89,6 +131,40 @@ struct MANGOS_DLL_DECL boss_jedogaAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
+        if (m_creature->GetHealth() < 50.0f && !m_bSacrifice)
+        {
+            // start sacrifice here
+            m_bSacrifice = true;
+        }
+
+        if (m_uiThundershockTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_THUNDERSHOCK : SPELL_THUNDERSHOCK_H);
+
+            m_uiThundershockTimer = 40000;
+        }
+        else
+            m_uiThundershockTimer -= uiDiff;
+
+        if (m_uiLightningBoltTimer < uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_LIGHTNING_BOLT : SPELL_LIGHTNING_BOLT_H);
+
+            m_uiLightningBoltTimer = 7000;
+        }
+        else
+            m_uiLightningBoltTimer -= uiDiff;
+
+        if (m_uiCycloneStrikeTimer < uiDiff)
+        {
+            DoCastSpellIfCan(m_creature->getVictim(), m_bIsRegularMode ? SPELL_CYCLONE_STRIKE : SPELL_CYCLONE_STRIKE_H);
+            m_uiCycloneStrikeTimer = 15000;
+        }
+        else
+            m_uiCycloneStrikeTimer -= uiDiff;
+
         DoMeleeAttackIfReady();
     }
 };
@@ -100,7 +176,7 @@ CreatureAI* GetAI_boss_jedoga(Creature* pCreature)
 
 void AddSC_boss_jedoga()
 {
-    Script *newscript;
+    Script* newscript;
 
     newscript = new Script;
     newscript->Name = "boss_jedoga";
