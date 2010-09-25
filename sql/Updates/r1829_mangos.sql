fbe901902b2b2593b8c5151733ef7ab8f23e1b3c
Reve <reve@scriptdev2.com>
2010-09-25 17:50:56 +0000

    [1832] Added less fail version of update queries.
    Added less fail version of update queries.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1832 5f9c896b-1e26-0410-94da-f77f675e2462

--- sql/Updates/r1829_mangos.sql
+++ sql/Updates/r1829_mangos.sql
@@ -1,5 +1,1 @@
-UPDATE creature_template SET ScriptName='' WHERE ScriptName='mob_shadow_of_aran';
-UPDATE creature_template SET ScriptName='' WHERE ScriptName='mob_ancient_core_hound';
-UPDATE creature_template SET ScriptName='' WHERE ScriptName='mob_coilfang_elite';
-UPDATE creature_template SET ScriptName='' WHERE ScriptName='mob_coilfang_strider';
-UPDATE creature_template SET ScriptName='' WHERE ScriptName='boss_ghazan';
+UPDATE creature_template SET ScriptName='' WHERE entry IN (11673, 18254, 22055, 22056);
