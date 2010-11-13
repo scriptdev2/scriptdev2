9e67fbc24feaf07959b144cee9a05bdadaef4cc9
Schmoozerd <schmoozerd@scriptdev2.com>
2010-11-13 19:02:03 +0000

    [1867] Add placeholders for instance_trial_of_the_champion, patch by Schmoozerd
    Add placeholders for instance_trial_of_the_champion, patch by Schmoozerd
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1867 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ scripts/northrend/crusaders_coliseum/trial_of_the_champion/trial_of_the_champion.h
@@ -0,0 +1,17 @@
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_TRIAL_OF_THE_CHAMPION_H
+#define DEF_TRIAL_OF_THE_CHAMPION_H
+
+enum
+{
+    MAX_ENCOUNTER                       = 3,
+
+    TYPE_GRAND_CHAMPIONS                = 0,
+    TYPE_ARGENT_CHAMPION                = 1,
+    TYPE_BLACK_KNIGHT                   = 2,
+};
+
+#endif
