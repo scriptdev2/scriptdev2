af0bfabd64eb52d867920ac083380c8136c1e32d
NoFantasy <nofantasy@nf.no>
2010-11-23 10:55:04 +0000

    [1887] Add new class ScriptedPetAI that can be used with certain pet types.
    Add new class ScriptedPetAI that can be used with certain pet types.
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1887 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ base/pet_ai.h
@@ -0,0 +1,40 @@
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef SC_PET_H
+#define SC_PET_H
+
+// Using CreatureAI for now. Might change later and use PetAI (need to export for dll first)
+class MANGOS_DLL_DECL ScriptedPetAI : public CreatureAI
+{
+    public:
+        explicit ScriptedPetAI(Creature* pCreature);
+        ~ScriptedPetAI() {}
+
+        void MoveInLineOfSight(Unit* /*pWho*/);
+
+        void AttackStart(Unit* /*pWho*/);
+
+        void AttackedBy(Unit* /*pAttacker*/);
+
+        void KilledUnit(Unit* /*pVictim*/) {}
+
+        void OwnerKilledUnit(Unit* /*pVictim*/) {}
+
+        void UpdateAI(const uint32 uiDiff);
+
+        // extras
+        void DoMeleeAttackIfReady();
+
+        virtual void Reset() {}
+
+        virtual void UpdatePetAI(const uint32 uiDiff);      // while in combat
+
+        virtual void UpdatePetOOCAI(const uint32 uiDiff) {} // when not in combat
+
+    protected:
+        void ResetPetCombat();
+};
+
+#endif
