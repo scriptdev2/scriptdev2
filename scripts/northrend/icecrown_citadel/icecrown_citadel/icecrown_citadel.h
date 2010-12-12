040052c8845b993bc174a7b361a0aad1d2886a91
Schmoozerd <schmoozerd@scriptdev2.com>
2010-12-12 12:19:08 +0000

    [1906] Add a bunch of new files to play with. Enjoy!
    Add a bunch of new files to play with. Enjoy!
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1906 5f9c896b-1e26-0410-94da-f77f675e2462

---
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
@@ -0,0 +1,3 @@
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
