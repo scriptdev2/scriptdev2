0216a4e41caab1ad16071f24550264dec39d91f1
cipherCOM <github@ciphercom.de>
2011-02-14 10:35:14 +0000

    [1979] Add cMake support. This requires Mangos 11167+ Patch by ciphercom
    Add cMake support. This requires Mangos 11167+ Patch by ciphercom
    
    git-svn-id: https://scriptdev2.svn.sourceforge.net/svnroot/scriptdev2@1979 5f9c896b-1e26-0410-94da-f77f675e2462

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOS 11136+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOS 11167+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
