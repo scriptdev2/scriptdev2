706a447661147f4b7d1eb8b14a51cbe5deb1109f
Schmoozerd <Schmoozerd@scriptdev2.com>
2011-03-22 10:19:58 +0100

    [2001] Update Copyright notice

--- scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
@@ -1,3 +1,3 @@
-/* Copyright (C) 2006 - 2011 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2006 - 2011 ScriptDev2 <http://www.scriptdev2.com/>
  * This program is free software licensed under GPL version 2
  * Please see the included DOCS/LICENSE.TXT for more information */
