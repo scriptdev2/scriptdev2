7624a3fee0f2d0953fff880c9f5605ba7b9c0a57
NeatElves <neatelves@scriptdev2.com>
2011-04-22 14:00:33 +0200

    [2038] Remove no longer needed script for npc_engineering_tele_trinket
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2037"
+ #define SD2_REVISION_NR "2038"
 #endif // __SD2_REVISION_NR_H__
---
+++ sql/updates/r2038_mangos.sql
@@ -0,0 +1,1 @@
+UPDATE creature_template SET ScriptName='' WHERE entry IN (14742,14743,21493,21494);
