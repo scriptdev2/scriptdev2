db1cefdecd696d0f3628fe97c5182de567f01205
Klark20 <klark20@online.de>
2011-05-06 09:39:24 +0200

    [2372] Lothos Riftwaker has created a smarter way to teleport players to Molten Core
    Signed-off-by: Klark20 <klark20@online.de>
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- scripts/eastern_kingdoms/searing_gorge.cpp
+++ scripts/eastern_kingdoms/searing_gorge.cpp
@@ -17,44 +17,15 @@
 /* ScriptData
 SDName: Searing_Gorge
 SD%Complete: 80
-SDComment: Quest support: 3377. Lothos Riftwaker teleport to Molten Core.
+SDComment: Quest support: 3377
 SDCategory: Searing Gorge
 EndScriptData */
 
 /* ContentData
-npc_lothos_riftwaker
 npc_zamael_lunthistle
 EndContentData */
 
 #include "precompiled.h"
-
-/*######
-## npc_lothos_riftwaker
-######*/
-
-bool GossipHello_npc_lothos_riftwaker(Player* pPlayer, Creature* pCreature)
-{
-    if (pCreature->isQuestGiver())
-        pPlayer->PrepareQuestMenu(pCreature->GetObjectGuid());
-
-    if (pPlayer->GetQuestRewardStatus(7487) || pPlayer->GetQuestRewardStatus(7848))
-        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport me to the Molten Core", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-
-    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
-
-    return true;
-}
-
-bool GossipSelect_npc_lothos_riftwaker(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
-{
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
-    {
-        pPlayer->CLOSE_GOSSIP_MENU();
-        pPlayer->TeleportTo(409, 1096.0f, -467.0f, -104.6f, 3.64f);
-    }
-
-    return true;
-}
 
 /*######
 ## npc_zamael_lunthistle
@@ -100,12 +71,6 @@ bool GossipSelect_npc_zamael_lunthistle(Player* pPlayer, Creature* pCreature, ui
 void AddSC_searing_gorge()
 {
     Script* pNewScript;
-
-    pNewScript = new Script;
-    pNewScript->Name = "npc_lothos_riftwaker";
-    pNewScript->pGossipHello =  &GossipHello_npc_lothos_riftwaker;
-    pNewScript->pGossipSelect = &GossipSelect_npc_lothos_riftwaker;
-    pNewScript->RegisterSelf();
 
     pNewScript = new Script;
     pNewScript->Name = "npc_zamael_lunthistle";
