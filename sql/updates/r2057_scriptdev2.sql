0663639345c6f25147c69eb0180f4276450a8c5a
PSZ <psz@scriptdev2.com>
2011-05-17 16:34:15 +0200

    [2115] Fix a bad query
    Signed-off-by: NoFantasy <nofantasy@nf.no>

--- sql/updates/r2057_scriptdev2.sql
+++ sql/updates/r2057_scriptdev2.sql
@@ -1,4 +1,4 @@
-DELETE FROM script_texts WHERE entry BETWEEN 1329018 AND 1329016;
+DELETE FROM script_texts WHERE entry BETWEEN -1329018 AND -1329016;
 INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
 (-1329016,'Today you have unmade what took me years to create! For this you shall all die by my hand!',0,1,0,0,'dathrohan SAY_AGGRO'),
 (-1329017,'You fools think you can defeat me so easily? Face the true might of the Nathrezim!',0,1,0,0,'dathrohan SAY_TRANSFORM'),
