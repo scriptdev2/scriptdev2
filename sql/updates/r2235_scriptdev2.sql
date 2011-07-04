157997dff5f64cb1cd993a45d8e69e7fd008bd42
Jethro <jethro.gibbs84@gmail.com>
2011-07-04 11:36:54 +0200

    [2236] Fix SQL update from rev 2235

--- sql/updates/r2235_scriptdev2.sql
+++ sql/updates/r2235_scriptdev2.sql
@@ -1,2 +1,3 @@
 DELETE FROM script_texts WHERE entry=-1469034;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
 (-1469034,'Run! They are coming.',0,1,0,0,'blackwing technician SAY_TECHNICIAN_RUN');
