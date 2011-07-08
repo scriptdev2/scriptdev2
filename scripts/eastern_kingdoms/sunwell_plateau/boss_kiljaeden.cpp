b1f83e8c5520ae1199e6a3fe99909cf96df30922
Schmoozerd <schmoozerd@scriptdev2.com>
2011-07-09 00:29:24 +0200

    [2243] Add texts for Kil'Jaeden (Sunwell)

--- scripts/eastern_kingdoms/sunwell_plateau/boss_kiljaeden.cpp
+++ scripts/eastern_kingdoms/sunwell_plateau/boss_kiljaeden.cpp
@@ -22,6 +22,42 @@ SDCategory: Sunwell Plateau
 EndScriptData */
 
 #include "precompiled.h"
+#include "sunwell_plateau.h"
+
+enum
+{
+    SAY_ORDER_1                 = -1580064,
+    SAY_ORDER_2                 = -1580065,
+    SAY_ORDER_3                 = -1580066,
+    SAY_ORDER_4                 = -1580067,
+    SAY_ORDER_5                 = -1580068,
+    SAY_EMERGE                  = -1580069,
+    SAY_SLAY_1                  = -1580070,
+    SAY_SLAY_2                  = -1580071,
+    SAY_REFLECTION_1            = -1580072,
+    SAY_REFLECTION_2            = -1580073,
+    SAY_DARKNESS_1              = -1580074,
+    SAY_DARKNESS_2              = -1580075,
+    SAY_DARKNESS_3              = -1580076,
+    SAY_PHASE_3                 = -1580077,
+    SAY_PHASE_4                 = -1580078,
+    SAY_PHASE_5                 = -1580079,
+    SAY_KALECGOS_INTRO          = -1580080,
+    SAY_KALECGOS_AWAKE_1        = -1580081,
+    SAY_ANVEENA_IMPRISONED      = -1580082,
+    SAY_KALECGOS_AWAKE_2        = -1580083,
+    SAY_ANVEENA_LOST            = -1580084,
+    SAY_KALECGOS_AWAKE_4        = -1580085,
+    SAY_ANVEENA_AWAKE           = -1580086,
+    SAY_KALECGOS_AWAKE_5        = -1580087,
+    SAY_ANVEENA_SACRIFICE       = -1580088,
+    SAY_KALECGOS_GOODBYE        = -1580089,
+    SAY_KALECGOS_ENCOURAGE      = -1580090,
+    SAY_KALECGOS_ORB_1          = -1580091,
+    SAY_KALECGOS_ORB_2          = -1580092,
+    SAY_KALECGOS_ORB_3          = -1580093,
+    SAY_KALECGOS_ORB_4          = -1580094,
+};
 
 void AddSC_boss_kiljaeden()
 {
