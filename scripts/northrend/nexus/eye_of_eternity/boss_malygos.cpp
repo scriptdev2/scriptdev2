f12b20682c6ecf0b298f10652b17506d169f57a8
Xfurry <mail@nomail.com>
2011-07-15 19:55:01 +0200

    [2247] Add texts for Malygos

--- scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
+++ scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
@@ -23,6 +23,46 @@ EndScriptData */
 
 #include "precompiled.h"
 
+enum
+{
+    SAY_INTRO_1             = -1616000,
+    SAY_INTRO_2             = -1616001,
+    SAY_INTRO_3             = -1616002,
+    SAY_INTRO_4             = -1616003,
+    SAY_INTRO_5             = -1616004,
+    SAY_AGGRO               = -1616005,
+    SAY_VORTEX              = -1616006,
+    SAY_SPARK_BUFF          = -1616007,
+    SAY_SLAY_1_A            = -1616008,
+    SAY_SLAY_1_B            = -1616009,
+    SAY_SLAY_1_C            = -1616010,
+    SAY_END_PHASE_1         = -1616011,
+    SAY_START_PHASE_2       = -1616012,
+    SAY_DEEP_BREATH         = -1616013,
+    SAY_SHELL               = -1616014,
+    SAY_SLAY_2_A            = -1616015,
+    SAY_SLAY_2_B            = -1616016,
+    SAY_SLAY_2_C            = -1616017,
+    SAY_END_PHASE_2         = -1616018,
+    SAY_INTRO_PHASE_3       = -1616019,
+    SAY_START_PHASE_3       = -1616020,
+    SAY_SLAY_3_A            = -1616021,
+    SAY_SLAY_3_B            = -1616022,
+    SAY_SLAY_3_C            = -1616023,
+    SAY_SURGE               = -1616024,
+    SAY_SPELL_1             = -1616025,
+    SAY_SPELL_2             = -1616026,
+    SAY_SPELL_3             = -1616027,
+    SAY_DEATH               = -1616028,
+    SAY_OUTRO_1             = -1616029,
+    SAY_OUTRO_2             = -1616030,
+    SAY_OUTRO_3             = -1616031,
+    SAY_OUTRO_4             = -1616032,
+
+    SAY_EMOTE_SPARK         = -1616033,
+    SAY_EMOTE_BREATH        = -1616034,
+};
+
 void AddSC_boss_malygos()
 {
 }
--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2246"
+ #define SD2_REVISION_NR "2247"
 #endif // __SD2_REVISION_NR_H__
