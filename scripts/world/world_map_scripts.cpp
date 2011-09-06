8f0dfbf2b23806d78a342d3f50a95cf7d236cb90
Schmoozerd <schmoozerd@scriptdev2.com>
2011-09-06 15:07:47 +0200

    [2274] Add support for spawning of npc 25003

--- scripts/world/world_map_scripts.cpp
+++ scripts/world/world_map_scripts.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: world_map_scripts
 SD%Complete: 0
-SDComment: Placeholder, Scripts for continents
+SDComment: Quest support: 11538
 SDCategory: World Map Scripts
 EndScriptData */
 
@@ -57,9 +57,53 @@ InstanceData* GetInstanceData_world_map_kalimdor(Map* pMap)
  *                     OUTLAND
  */
 
+enum
+{
+    NPC_EMISSARY_OF_HATE        = 25003,
+    NPC_IRESPEAKER              = 24999,
+    NPC_UNLEASHED_HELLION       = 25002,
+};
+
+static const float aSpawnLocations[1][4] =
+{
+    {12583.019531f, -6916.194336f, 4.601806f, 6.182446f}    // Emissary of Hate, guesswork
+};
+
 struct MANGOS_DLL_DECL world_map_outland : public ScriptedMap
 {
-    world_map_outland(Map* pMap) : ScriptedMap(pMap) {}
+    world_map_outland(Map* pMap) : ScriptedMap(pMap) { Initialize(); }
+
+    uint8 m_uiEmissaryOfHate_KilledAddCount;
+
+    void Initialize()
+    {
+        m_uiEmissaryOfHate_KilledAddCount = 0;
+    }
+
+    void OnCreatureCreate(Creature* pCreature)
+    {
+        if (pCreature->GetEntry() == NPC_EMISSARY_OF_HATE)
+            m_mNpcEntryGuidStore[NPC_EMISSARY_OF_HATE] = pCreature->GetObjectGuid();
+    }
+
+    void OnCreatureDeath(Creature* pCreature)
+    {
+        switch (pCreature->GetEntry())
+        {
+            case NPC_IRESPEAKER:
+            case NPC_UNLEASHED_HELLION:
+                if (!GetSingleCreatureFromStorage(NPC_EMISSARY_OF_HATE, true))
+                {
+                    ++m_uiEmissaryOfHate_KilledAddCount;
+                    if (m_uiEmissaryOfHate_KilledAddCount == 6)
+                    {
+                        pCreature->SummonCreature(NPC_EMISSARY_OF_HATE, aSpawnLocations[0][0], aSpawnLocations[0][1], aSpawnLocations[0][2], aSpawnLocations[0][3], TEMPSUMMON_DEAD_DESPAWN, 0);
+                        m_uiEmissaryOfHate_KilledAddCount = 0;
+                    }
+                }
+                break;
+        }
+    }
 
     void SetData(uint32 uiType, uint32 uiData) {}
 };
