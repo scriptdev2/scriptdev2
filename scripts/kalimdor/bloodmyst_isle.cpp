b1c571621512a7b72f4365c52cb85b50966d2ca0
DaC <nighoo@gmail.com>
2011-09-11 15:34:10 +0200

    [2278] Fix possible spawns for Webbed Creature in Bloodmyst Isle
    Note: There are spells related to this event, likely whole script should be changed to a spell effect and eventAi script
    
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- scripts/kalimdor/bloodmyst_isle.cpp
+++ scripts/kalimdor/bloodmyst_isle.cpp
@@ -33,7 +33,7 @@ EndContentData */
 ######*/
 
 //possible creatures to be spawned
-const uint32 possibleSpawns[32] = {17322, 17661, 17496, 17522, 17340, 17352, 17333, 17524, 17654, 17348, 17339, 17345, 17359, 17353, 17336, 17550, 17330, 17701, 17321, 17680, 17325, 17320, 17683, 17342, 17715, 17334, 17341, 17338, 17337, 17346, 17344, 17327};
+const uint32 possibleSpawns[31] = {17322, 17661, 17496, 17522, 17340, 17352, 17333, 17524, 17654, 17348, 17339, 17345, 17359, 17353, 17336, 17550, 17330, 17701, 17321, 17325, 17320, 17683, 17342, 17715, 17334, 17341, 17338, 17337, 17346, 17344, 17327};
 
 struct MANGOS_DLL_DECL mob_webbed_creatureAI : public ScriptedAI
 {
@@ -56,7 +56,7 @@ struct MANGOS_DLL_DECL mob_webbed_creatureAI : public ScriptedAI
                 break;
             case 1:
             case 2:
-                spawnCreatureID = possibleSpawns[urand(0, 31)];
+                spawnCreatureID = possibleSpawns[urand(0, 30)];
                 break;
         }
 
--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2277"
+ #define SD2_REVISION_NR "2278"
 #endif // __SD2_REVISION_NR_H__
