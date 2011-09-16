2ef96c517f05bc94717c520d5b1ddc39c555b058
Reamer <philipp.dallig@web.de>
2011-09-16 22:28:27 +0200

    [2285] Fix IsEncounterInProgress for Naxxramas
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>

--- scripts/northrend/naxxramas/instance_naxxramas.cpp
+++ scripts/northrend/naxxramas/instance_naxxramas.cpp
@@ -220,9 +220,15 @@ void instance_naxxramas::OnCreatureDeath(Creature* pCreature)
 
 bool instance_naxxramas::IsEncounterInProgress() const
 {
-    for (uint8 i = 0; i < TYPE_KELTHUZAD; ++i)
-        if (m_auiEncounter[i] == IN_PROGRESS || m_auiEncounter[i] == SPECIAL)
+    for (uint8 i = 0; i <= TYPE_KELTHUZAD; ++i)
+    {
+        if (m_auiEncounter[i] == IN_PROGRESS)
             return true;
+    }
+
+    // Some Encounters use SPECIAL while in progress
+    if (m_auiEncounter[TYPE_GOTHIK] == SPECIAL)
+        return true;
 
     return false;
 }
