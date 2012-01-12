f30365814a6798a0c635f56add281c83f0094e20
Xfurry <xfurry@scriptdev2.com>
2012-01-12 22:29:26 +0200

    [2382] Add placeholder for Oculus

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2381"
+ #define SD2_REVISION_NR "2382"
 #endif // __SD2_REVISION_NR_H__
--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -349,6 +349,9 @@ extern void AddSC_boss_keristrasza();
 extern void AddSC_boss_ormorok();
 extern void AddSC_boss_telestra();
 extern void AddSC_instance_nexus();
+extern void AddSC_boss_eregos();                            // nexus, oculus
+extern void AddSC_boss_urom();
+extern void AddSC_instance_oculus();
 extern void AddSC_boss_sartharion();                        // obsidian_sanctum
 extern void AddSC_instance_obsidian_sanctum();
 extern void AddSC_boss_baltharus();                         // ruby_sanctum
@@ -841,6 +844,9 @@ void AddScripts()
     AddSC_boss_ormorok();
     AddSC_boss_telestra();
     AddSC_instance_nexus();
+    AddSC_boss_eregos();                                    // nexus, oculus
+    AddSC_boss_urom();
+    AddSC_instance_oculus();
     AddSC_boss_sartharion();                                // obsidian_sanctum
     AddSC_instance_obsidian_sanctum();
     AddSC_boss_baltharus();                                 // ruby_sanctum
