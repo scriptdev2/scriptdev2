ba3d2665c329b38dbe8fa45a02c41d23dbe1fbc3
Xfurry <xfurry@scriptdev2.com>
2012-01-12 22:29:26 +0200

    Backport [2382] to keep MakeFiles in sync
    Add placeholder for Oculus
    
    For Documentation purpose:
    The last 4 commits were done with 'git cherry-pick <CommitHash>'
    One was picked for compile fix, one was picked because ICC related, and the other two to keep the "makefiles" in sync for easier and faster compile.
    I didn't merge the master branch, to keep the history nice and clear

---
+++ scripts/northrend/nexus/oculus/oculus.h
@@ -0,0 +1,3 @@
+/* Copyright (C) 2006 - 2012 ScriptDev2 <http://www.scriptdev2.com/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
