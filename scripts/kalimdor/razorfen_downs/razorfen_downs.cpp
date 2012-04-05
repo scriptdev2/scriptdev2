9f6c1abb023a7a868cb5f9ddb5806993c9fa4e7e
NeatElves <neatelves@scriptdev2.com>
2012-04-05 23:45:32 +0300

    [2499] Remove script for npc 8696. Should be already handled in DB
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- scripts/kalimdor/razorfen_downs/razorfen_downs.cpp
+++ scripts/kalimdor/razorfen_downs/razorfen_downs.cpp
@@ -17,63 +17,16 @@
 /* ScriptData
 SDName: Razorfen_Downs
 SD%Complete: 100
-SDComment: Support for Henry Stern(2 recipes), Quest 3525
+SDComment: Quest 3525
 SDCategory: Razorfen Downs
 EndScriptData */
 
 /* ContentData
-npc_henry_stern
 npc_belnistrasz
 EndContentData */
 
 #include "precompiled.h"
 #include "escort_ai.h"
-
-/*###
-# npc_henry_stern
-####*/
-
-enum
-{
-    SPELL_GOLDTHORN_TEA                         = 13028,
-    SPELL_TEACHING_GOLDTHORN_TEA                = 13029,
-    SPELL_MIGHT_TROLLS_BLOOD_POTION             = 3451,
-    SPELL_TEACHING_MIGHTY_TROLLS_BLOOD_POTION   = 13030,
-    GOSSIP_TEXT_TEA_ANSWER                      = 2114,
-    GOSSIP_TEXT_POTION_ANSWER                   = 2115,
-};
-
-#define GOSSIP_ITEM_TEA     "Teach me the cooking recipe"
-#define GOSSIP_ITEM_POTION  "Teach me the alchemy recipe"
-
-bool GossipHello_npc_henry_stern (Player* pPlayer, Creature* pCreature)
-{
-    if (pPlayer->GetBaseSkillValue(SKILL_COOKING) >= 175 && !pPlayer->HasSpell(SPELL_GOLDTHORN_TEA))
-        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_TEA, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-
-    if (pPlayer->GetBaseSkillValue(SKILL_ALCHEMY) >= 180 && !pPlayer->HasSpell(SPELL_MIGHT_TROLLS_BLOOD_POTION))
-        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_POTION, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-
-    pPlayer->SEND_GOSSIP_MENU(pPlayer->GetGossipTextId(pCreature), pCreature->GetObjectGuid());
-    return true;
-}
-
-bool GossipSelect_npc_henry_stern (Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
-{
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
-    {
-        pCreature->CastSpell(pPlayer, SPELL_TEACHING_GOLDTHORN_TEA, true);
-        pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXT_TEA_ANSWER, pCreature->GetObjectGuid());
-    }
-
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 2)
-    {
-        pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXT_POTION_ANSWER, pCreature->GetObjectGuid());
-        pCreature->CastSpell(pPlayer, SPELL_TEACHING_MIGHTY_TROLLS_BLOOD_POTION, true);
-    }
-
-    return true;
-}
 
 /*###
 # npc_belnistrasz
@@ -337,12 +290,6 @@ bool QuestAccept_npc_belnistrasz(Player* pPlayer, Creature* pCreature, const Que
 void AddSC_razorfen_downs()
 {
     Script* pNewScript;
-
-    pNewScript = new Script;
-    pNewScript->Name = "npc_henry_stern";
-    pNewScript->pGossipHello = &GossipHello_npc_henry_stern;
-    pNewScript->pGossipSelect = &GossipSelect_npc_henry_stern;
-    pNewScript->RegisterSelf();
 
     pNewScript = new Script;
     pNewScript->Name = "npc_belnistrasz";
