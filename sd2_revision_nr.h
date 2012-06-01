0f7beacfcb599034c702ef6c2e701241d4066fff
Xfurry <xfurry@scriptdev2.com>
2012-06-02 00:03:20 +0300

    [2578] Implement script for Amanitar - Ahn'kahet
    Based on the work of Tasssadar
    
    Notes:
    Mushrooms summoning may not be 100% correct - requires further research
    Spells 57055 and 56648 require some core stacking additions

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2577"
+ #define SD2_REVISION_NR "2578"
 #endif // __SD2_REVISION_NR_H__
---
+++ sql/updates/r2578_mangos.sql
@@ -0,0 +1,2 @@
+UPDATE creature_template SET ScriptName='boss_amanitar' WHERE entry=30258;
+UPDATE creature_template SET ScriptName='npc_amanitar_mushroom' WHERE entry IN (30391,30435);
