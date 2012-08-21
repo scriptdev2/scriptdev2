eca7e092bff776b9cf503c24524157347fd25722
michalpolko <michalpolko@o2.pl>
2012-08-21 21:05:29 +0200

    [2692] Drop scripts for Valithria summoned creatures (they can be handled in EAI).

---
+++ sql/updates/r2692_mangos.sql
@@ -0,0 +1,5 @@
+UPDATE creature_template SET ScriptName='' WHERE entry=37886;
+UPDATE creature_template SET ScriptName='' WHERE entry=37934;
+UPDATE creature_template SET ScriptName='' WHERE entry=37868;
+UPDATE creature_template SET ScriptName='' WHERE entry=36791;
+UPDATE creature_template SET ScriptName='' WHERE entry=37863;
