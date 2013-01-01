59091740267d5d623593bf2a2722cae7adb334d0
Xfurry <radu_cantor@yahoo.com>
2013-01-01 15:23:16 +0200

    [2781] Happy New Year 2013!

--- scripts/outland/black_temple/boss_warlord_najentus.cpp
+++ scripts/outland/black_temple/boss_warlord_najentus.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2012 ScriptDev2 <http://www.scriptdev2.com/>
+/* Copyright (C) 2006 - 2013 ScriptDev2 <http://www.scriptdev2.com/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
--- system/system.cpp
+++ system/system.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2012 ScriptDev2 <http://www.scriptdev2.com/>
+/* Copyright (C) 2006 - 2013 ScriptDev2 <http://www.scriptdev2.com/>
 * This program is free software licensed under GPL version 2
 * Please see the included DOCS/LICENSE.TXT for more information */
 
