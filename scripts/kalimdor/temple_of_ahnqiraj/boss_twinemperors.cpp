8e52a376becdcf3758cdce82edea7b1d76b0315a
Xfurry <xfurry@scriptdev2.com>
2013-04-09 22:38:02 +0300

    [2860] Allow Twin Emperors to use HealdedBy function

--- scripts/kalimdor/temple_of_ahnqiraj/boss_twinemperors.cpp
+++ scripts/kalimdor/temple_of_ahnqiraj/boss_twinemperors.cpp
@@ -105,6 +105,21 @@ struct MANGOS_DLL_DECL boss_twin_emperorsAI : public ScriptedAI
         }
     }
 
+    // Workaround for the shared health pool
+    void HealedBy(Unit * pHealer, uint32& uiHealedAmount) override
+    {
+        if (!m_pInstance)
+            return;
+
+        if (Creature* pTwin = m_pInstance->GetSingleCreatureFromStorage(m_creature->GetEntry() == NPC_VEKLOR ? NPC_VEKLOR : NPC_VEKNILASH))
+        {
+            float fHealPercent = ((float)uiHealedAmount) / ((float)m_creature->GetMaxHealth());
+            uint32 uiTwinHeal = (uint32)(fHealPercent * ((float)pTwin->GetMaxHealth()));
+            uint32 uiTwinHealth = pTwin->GetHealth() + uiTwinHeal;
+            pTwin->SetHealth(uiTwinHealth < pTwin->GetMaxHealth() ? uiTwinHealth : pTwin->GetMaxHealth());
+        }
+    }
+
     void Aggro(Unit* /*pWho*/) override
     {
         if (m_pInstance)
--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "2859"
+ #define SD2_REVISION_NR "2860"
 #endif // __SD2_REVISION_NR_H__
