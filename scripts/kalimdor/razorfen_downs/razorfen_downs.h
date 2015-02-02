86d08b9a0f41f9dba6cbef68ff48dc19f395745d
cala <calaftp@free.fr>
2015-02-02 23:12:07 +0100

    [3095] Add placeholder for Razorfen Downs instance
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

---
+++ scripts/kalimdor/razorfen_downs/razorfen_downs.h
@@ -0,0 +1,10 @@
+/* This file is part of the ScriptDev2 Project. See AUTHORS file for Copyright information
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_RD_H
+#define DEF_RD_H
+
+
+
+#endif
--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -184,7 +184,8 @@ extern void AddSC_instance_dire_maul();
 extern void AddSC_boss_noxxion();                           // maraudon
 extern void AddSC_boss_onyxia();                            // onyxias_lair
 extern void AddSC_instance_onyxias_lair();
-extern void AddSC_razorfen_downs();                         // razorfen_downs
+extern void AddSC_instance_razorfen_downs();                // razorfen_downs
+extern void AddSC_razorfen_downs();
 extern void AddSC_instance_razorfen_kraul();                // razorfen_kraul
 extern void AddSC_razorfen_kraul();
 extern void AddSC_boss_ayamiss();                           // ruins_of_ahnqiraj
@@ -649,7 +650,8 @@ void AddScripts()
     AddSC_boss_noxxion();                                   // maraudon
     AddSC_boss_onyxia();                                    // onyxias_lair
     AddSC_instance_onyxias_lair();
-    AddSC_razorfen_downs();                                 // razorfen_downs
+    AddSC_instance_razorfen_downs();                        // razorfen_downs
+    AddSC_razorfen_downs();
     AddSC_instance_razorfen_kraul();                        // razorfen_kraul
     AddSC_razorfen_kraul();
     AddSC_boss_ayamiss();                                   // ruins_of_ahnqiraj
