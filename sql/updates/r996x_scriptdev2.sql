902fd0a8bfc73a74416491856de3641d72f73186
Cala <calaftp@free.fr>
2015-08-29 14:47:07 +0200

    BRD Grim Guzzler rework part 3
    * Add the bar patrol event when Ribbly is killed (should also be true
    for Plugger but this will come in next commit)
    * Add a few basis ground for Plugger script which will come soon

---
+++ sql/updates/r996x_scriptdev2.sql
@@ -0,0 +1,5 @@
+DELETE FROM script_texts WHERE entry BETWEEN -1230047 AND -1230049;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1230047,'I\m out of here!',0,0,0,60,'rocknot SAY_ROCKNOT_DESPAWN'),
+(-1230048,'HALT! There were...reports...of a...disturbance.',0,1,0,0,'Fireguard Destroyer YELL_PATROL_1'),
+(-1230049,'We...are...investigating.',0,1,0,0,'Fireguard Destroyer YELL_PATROL_2');
